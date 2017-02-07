ee74848143d02937892fd0ade3b22779c875caee
Mikita Hladkikh <mikita_hladkikh@epam.com>
2017-02-07 15:22:07 +0300

    B-28929 Refactoring of Technical debt: use test module from dist-common

--- dist-foreign-domain/src/test/java/com/copyright/rup/dist/foreign/domain/DomainVerifierTest.java
+++ dist-foreign-domain/src/test/java/com/copyright/rup/dist/foreign/domain/DomainVerifierTest.java
@@ -1,31 +1,13 @@
 package com.copyright.rup.dist.foreign.domain;
 
-import static org.junit.Assert.assertTrue;
+import com.copyright.rup.dist.common.test.TestUtils;
 
-import com.openpojo.random.RandomFactory;
-import com.openpojo.reflection.PojoClass;
-import com.openpojo.reflection.impl.PojoClassFactory;
-import com.openpojo.validation.PojoValidator;
-import com.openpojo.validation.rule.impl.GetterMustExistRule;
-import com.openpojo.validation.rule.impl.NoFieldShadowingRule;
-import com.openpojo.validation.rule.impl.NoStaticExceptFinalRule;
-import com.openpojo.validation.rule.impl.SetterMustExistRule;
-import com.openpojo.validation.test.Tester;
-import com.openpojo.validation.test.impl.GetterTester;
-import com.openpojo.validation.test.impl.SetterTester;
-import com.openpojo.validation.utils.ValidationHelper;
-
-import org.apache.commons.lang3.StringUtils;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.util.Arrays;
 import java.util.Collection;
-
-import nl.jqno.equalsverifier.EqualsVerifier;
-import nl.jqno.equalsverifier.Warning;
 
 /**
  * Verifies {@link Object#equals(Object)}, {@link Object#hashCode()},
@@ -40,8 +22,6 @@ import nl.jqno.equalsverifier.Warning;
 @RunWith(Parameterized.class)
 public class DomainVerifierTest {
 
-    private static PojoValidator sPojoValidator;
-
     private Class classToVerify;
 
     /**
@@ -53,20 +33,6 @@ public class DomainVerifierTest {
         this.classToVerify = classToVerify;
     }
 
-    @BeforeClass
-    public static void beforeClass() {
-        sPojoValidator = new PojoValidator();
-
-        sPojoValidator.addRule(new NoStaticExceptFinalRule());
-        sPojoValidator.addRule(new NoFieldShadowingRule());
-        sPojoValidator.addRule(new GetterMustExistRule());
-        sPojoValidator.addRule(new SetterMustExistRule());
-
-        sPojoValidator.addTester(new SetterTester());
-        sPojoValidator.addTester(new GetterTester());
-        sPojoValidator.addTester(new EqualsHashCodeTester());
-        sPojoValidator.addTester(new ToStringTester());
-    }
 
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
@@ -80,39 +46,6 @@ public class DomainVerifierTest {
 
     @Test
     public void testPojoStructureAndBehavior() {
-        sPojoValidator.runValidation(PojoClassFactory.getPojoClass(classToVerify));
-    }
-
-    private static class EqualsHashCodeTester implements Tester {
-
-        @Override
-        public void run(PojoClass pojoClass) {
-            EqualsVerifier.forClass(pojoClass.getClazz())
-                .suppress(Warning.NONFINAL_FIELDS)
-                .usingGetClass()
-                .verify();
-        }
-    }
-
-    private static class ToStringTester implements Tester {
-
-        @Override
-        public void run(PojoClass pojoClass) {
-            Object classInstance = ValidationHelper.getMostCompleteInstance(pojoClass);
-            pojoClass.getPojoFields().stream()
-                .filter(pojoField -> !pojoField.isStatic())
-                .forEach(pojoField -> {
-                    Object fieldValue = RandomFactory.getRandomValue(pojoField.getType());
-                    pojoField.set(classInstance, fieldValue);
-                });
-            String stringRepresentation = classInstance.toString();
-            assertTrue(StringUtils.contains(stringRepresentation, pojoClass.getClazz().getSimpleName()));
-            pojoClass.getPojoFields().stream()
-                .filter(pojoField -> !pojoField.isStatic())
-                .forEach(pojoField -> {
-                    assertTrue(StringUtils.contains(stringRepresentation, pojoField.getName()));
-                    assertTrue(StringUtils.contains(stringRepresentation, pojoField.get(classInstance).toString()));
-                });
-        }
+        TestUtils.validatePojo(classToVerify);
     }
 }
