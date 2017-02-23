22725ecc58762d50c83d5c7e4bb02a1ab7b93bec
MikalaiBezmen <Mikalai_Bezmen@epam.com>
2017-02-23 10:56:11 +0300

    B-29460 Integrate with PRM to get RH and RRO names: Implement logic for storing rightsholders information into database

--- dist-foreign-repository/src/main/java/com/copyright/rup/dist/foreign/repository/impl/RightsholderRepository.java
+++ dist-foreign-repository/src/main/java/com/copyright/rup/dist/foreign/repository/impl/RightsholderRepository.java
@@ -1,5 +1,7 @@
 package com.copyright.rup.dist.foreign.repository.impl;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import com.copyright.rup.dist.common.domain.Rightsholder;
 import com.copyright.rup.dist.common.repository.BaseRepository;
 import com.copyright.rup.dist.foreign.repository.api.IRightsholderRepository;
@@ -23,5 +25,17 @@ public class RightsholderRepository extends BaseRepository implements IRightshol
     @Override
     public List<Rightsholder> findRros() {
         return selectList("IRightsholderMapper.findRros");
+    }
+
+    @Override
+    public void insert(Rightsholder rightsholder) {
+        insert("IRightsholderMapper.insert", checkNotNull(rightsholder));
+    }
+
+    /**
+     * @return list of all {@link Rightsholder}s from DB.
+     */
+    List<Rightsholder> findAll() {
+        return selectList("IRightsholderMapper.findAll");
     }
 }
