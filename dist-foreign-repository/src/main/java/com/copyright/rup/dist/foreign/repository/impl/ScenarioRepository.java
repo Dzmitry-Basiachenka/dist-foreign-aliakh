a3756c218ed585d363bf819c9ff26d3064dd9497
IharSurovau1 <Ihar_Suvorau@epam.com>
2017-03-13 09:45:58 +0300

    B-29575 Create an FAS Scenario: Implement repository layer for scenario to work with DB

---
+++ dist-foreign-repository/src/main/java/com/copyright/rup/dist/foreign/repository/impl/ScenarioRepository.java
@@ -0,0 +1,41 @@
+package com.copyright.rup.dist.foreign.repository.impl;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.copyright.rup.dist.common.repository.BaseRepository;
+import com.copyright.rup.dist.foreign.domain.Scenario;
+import com.copyright.rup.dist.foreign.repository.api.IScenarioRepository;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Implementation of {@link IScenarioRepository} for MyBatis.
+ * <p/>
+ * Copyright (C) 2017 copyright.com
+ * <p/>
+ * Date: 03/15/17
+ *
+ * @author Ihar Suvorau
+ */
+@Repository
+public class ScenarioRepository extends BaseRepository implements IScenarioRepository {
+
+    @Override
+    public void insert(Scenario scenario) {
+        insert("IScenarioMapper.insert", checkNotNull(scenario));
+    }
+
+    @Override
+    public int getCountByName(String name) {
+        checkArgument(StringUtils.isNotBlank(name));
+        return selectOne("IScenarioMapper.getCountByName", name);
+    }
+
+    @Override
+    public Scenario findById(String id) {
+        checkArgument(StringUtils.isNotBlank(id));
+        return selectOne("IScenarioMapper.findById", id);
+    }
+}
