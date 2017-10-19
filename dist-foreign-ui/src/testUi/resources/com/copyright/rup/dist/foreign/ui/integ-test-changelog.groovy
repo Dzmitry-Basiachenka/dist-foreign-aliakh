1b4a645cb6b42c67ee9ce998d0db07bbdde885e1
AliaksandraBayanouskaya <aliaksandra_bayanouskaya@epam.com>
2017-10-19 16:11:59 +0300

    B-34404 FDA: Integrate with PRM to get Payee for each detail: implement liquibase script to add df_rightsholder_uid column to df_rightsholder table

--- dist-foreign-ui/src/testUi/resources/com/copyright/rup/dist/foreign/ui/integ-test-changelog.groovy
+++ dist-foreign-ui/src/testUi/resources/com/copyright/rup/dist/foreign/ui/integ-test-changelog.groovy
@@ -150,41 +150,49 @@ databaseChangeLog {
         comment('Inserting Rightsholders for ui tests')
 
         insert(schemaName: dbAppsSchema, tableName: 'df_rightsholder') {
+            column(name: 'df_rightsholder_uid', value: 'e9c9f51b-6048-4474-848a-2db1c410e463')
             column(name: 'rh_account_number', value: '1000002797')
             column(name: 'name', value: 'British Film Institute (BFI)')
         }
 
         insert(schemaName: dbAppsSchema, tableName: 'df_rightsholder') {
+            column(name: 'df_rightsholder_uid', value: '5bcf2c37-2f32-48e9-90fe-c9d75298eeed')
             column(name: 'rh_account_number', value: '1000002859')
             column(name: 'name', value: 'John Wiley & Sons - Books')
         }
 
         insert(schemaName: dbAppsSchema, tableName: 'df_rightsholder') {
+            column(name: 'df_rightsholder_uid', value: '8a0dbf78-d9c9-49d9-a895-05f55cfc8329')
             column(name: 'rh_account_number', value: '1000005413')
             column(name: 'name', value: 'Kluwer Academic Publishers - Dordrecht')
         }
 
         insert(schemaName: dbAppsSchema, tableName: 'df_rightsholder') {
+            column(name: 'df_rightsholder_uid', value: '6eb8ddf3-f08e-4c17-a0c5-5173d43a1625')
             column(name: 'rh_account_number', value: '1000008666')
             column(name: 'name', value: 'CCH')
         }
 
         insert(schemaName: dbAppsSchema, tableName: 'df_rightsholder') {
+            column(name: 'df_rightsholder_uid', value: '9905f006-a3e1-4061-b3d4-e7ece191103f')
             column(name: 'rh_account_number', value: '1000009997')
             column(name: 'name', value: 'IEEE - Inst of Electrical and Electronics Engrs')
         }
 
         insert(schemaName: dbAppsSchema, tableName: 'df_rightsholder') {
+            column(name: 'df_rightsholder_uid', value: '46754660-b627-46b9-a782-3f703b6853c7')
             column(name: 'rh_account_number', value: '2000017004')
             column(name: 'name', value: 'Access Copyright, The Canadian Copyright Agency')
         }
 
         insert(schemaName: dbAppsSchema, tableName: 'df_rightsholder') {
+            column(name: 'df_rightsholder_uid', value: '05c4714b-291d-4e38-ba4a-35307434acfb')
             column(name: 'rh_account_number', value: '7000813806')
             column(name: 'name', value: 'CADRA, Centro de Administracion de Derechos Reprograficos, Asociacion Civil')
         }
 
         insert(schemaName: dbAppsSchema, tableName: 'df_rightsholder') {
+            column(name: 'df_rightsholder_uid', value: 'ff8b9ac9-5fca-4d57-b74e-26da209c1040')
             column(name: 'rh_account_number', value: '7001440663')
             column(name: 'name', value: 'JAACC, Japan Academic Association for Copyright Clearance [T]')
         }
