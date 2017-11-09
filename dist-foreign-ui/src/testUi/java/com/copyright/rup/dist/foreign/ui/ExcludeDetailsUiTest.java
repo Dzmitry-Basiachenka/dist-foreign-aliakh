04c538b9dcf9d004300b998e567f202971f50ee8
Uladzislau Shalamitski <Uladzislau_Shalamitski@epam.com>
2017-11-09 02:51:27 -0500

    B-37967 UI for Identify and exclude details for rightsholders that roll up to the source RRO: Add empty class for verification of usages exclusion

---
+++ dist-foreign-ui/src/testUi/java/com/copyright/rup/dist/foreign/ui/ExcludeDetailsUiTest.java
@@ -0,0 +1,13 @@
+package com.copyright.rup.dist.foreign.ui;
+
+/**
+ * Verifies exclusion of details.
+ * <p>
+ * Copyright (C) 2017 copyright.com
+ * <p>
+ * Date: 11/9/2017
+ *
+ * @author Uladzislau_Shalamitski
+ */
+public class ExcludeDetailsUiTest {
+}
