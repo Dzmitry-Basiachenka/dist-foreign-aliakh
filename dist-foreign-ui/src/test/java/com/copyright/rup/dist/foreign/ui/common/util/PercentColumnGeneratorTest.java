b86b97d303d83bb7eed198049f985d296c3267fb
AliaksandraBayanouskaya <aliaksandra_bayanouskaya@epam.com>
2017-11-14 12:59:00 +0300

    B-37945 FDA: Calculate the service fee on each eligible FAS detail: implement unit test for PercentColumnGenerator

--- dist-foreign-ui/src/test/java/com/copyright/rup/dist/foreign/ui/common/util/PercentColumnGeneratorTest.java
+++ dist-foreign-ui/src/test/java/com/copyright/rup/dist/foreign/ui/common/util/PercentColumnGeneratorTest.java
@@ -8,7 +8,6 @@ import static org.powermock.api.easymock.PowerMock.replay;
 import static org.powermock.api.easymock.PowerMock.verify;
 
 import com.copyright.rup.vaadin.ui.VaadinUtils;
-
 import com.vaadin.ui.Table;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
@@ -16,33 +15,35 @@ import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.math.BigDecimal;
+
 /**
- * Verifies {@link IntegerColumnGenerator}.
+ * Verifies {@link PercentColumnGenerator}.
  * <p/>
  * Copyright (C) 2017 copyright.com
  * <p/>
- * Date: 02/08/2017
+ * Date: 14/11/2017
  *
- * @author Mikalai Bezmen
+ * @author Aliaksandra Bayanouskaya
  */
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(VaadinUtils.class)
-public class IntegerColumnGeneratorTest {
+public class PercentColumnGeneratorTest {
 
     private static final String ITEM_ID = "item id";
     private static final String COLUMN_ID = "column id";
 
     @Test
-    public void testGenerateCellForIntegerValue() {
+    public void testGenerateCellForBigDecimalValue() {
         Table table = new Table();
         mockStatic(VaadinUtils.class);
-        IntegerColumnGenerator integerColumnGenerator = new IntegerColumnGenerator();
-        expect(VaadinUtils.getContainerPropertyValue(table, ITEM_ID, COLUMN_ID, Integer.class))
-            .andReturn(2015).once();
+        PercentColumnGenerator percentColumnGenerator = new PercentColumnGenerator();
+        expect(VaadinUtils.getContainerPropertyValue(table, ITEM_ID, COLUMN_ID, BigDecimal.class))
+            .andReturn(new BigDecimal("0.32000")).once();
         replay(VaadinUtils.class);
-        Object result = integerColumnGenerator.generateCell(table, ITEM_ID, COLUMN_ID);
+        Object result = percentColumnGenerator.generateCell(table, ITEM_ID, COLUMN_ID);
         assertNotNull(result);
-        assertEquals("2015", result);
+        assertEquals("32.0", result);
         verify(VaadinUtils.class);
     }
 
@@ -50,11 +51,11 @@ public class IntegerColumnGeneratorTest {
     public void testGenerateCellForNullValue() {
         Table table = new Table();
         mockStatic(VaadinUtils.class);
-        IntegerColumnGenerator integerColumnGenerator = new IntegerColumnGenerator();
-        expect(VaadinUtils.getContainerPropertyValue(table, ITEM_ID, COLUMN_ID, Integer.class))
+        PercentColumnGenerator percentColumnGenerator = new PercentColumnGenerator();
+        expect(VaadinUtils.getContainerPropertyValue(table, ITEM_ID, COLUMN_ID, BigDecimal.class))
             .andReturn(null).once();
         replay(VaadinUtils.class);
-        Object result = integerColumnGenerator.generateCell(table, ITEM_ID, COLUMN_ID);
+        Object result = percentColumnGenerator.generateCell(table, ITEM_ID, COLUMN_ID);
         assertNotNull(result);
         assertEquals(StringUtils.EMPTY, result);
         verify(VaadinUtils.class);
