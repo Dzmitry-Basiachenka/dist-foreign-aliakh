870dce470cea91d025cb6af87409628daec396b4
PavelLiakh <Pavel_Liakh@epam.com>
2018-01-19 02:43:14 -0500

    B-26839 Tech Debt, Refactoring, and Demo Feedback: add UI test to check user cannot delete Submitted, Approved, Sent to LM scenarios according to QA test case

--- dist-foreign-repository/src/main/java/com/copyright/rup/dist/foreign/repository/impl/UsageArchiveRepository.java
+++ dist-foreign-repository/src/main/java/com/copyright/rup/dist/foreign/repository/impl/UsageArchiveRepository.java
@@ -93,4 +93,9 @@ public class UsageArchiveRepository extends BaseRepository implements IUsageArch
             throw new RupRuntimeException(e);
         }
     }
+
+    @Override
+    public void deleteArchivedUsagesByScenarioId(String scenarioId){
+        delete("IUsageArchiveMapper.deleteArchivedUsagesByScenarioId", scenarioId);
+    }
 }
