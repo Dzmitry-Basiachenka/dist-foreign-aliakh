7c7b77a71e3899e3c19993dfeb6106b94922b079
DaryaBaraukova <Darya_Baraukova@epam.com>
2018-02-23 12:01:50 +0300

    B-41176 FDA: get paid information from LM: implement integration test to verify consuming of paid information from LM

---
+++ dist-foreign-service/src/testInteg/java/com/copyright/rup/dist/foreign/service/impl/ReceivePaidUsagesFromLmTest.java
@@ -0,0 +1,74 @@
+package com.copyright.rup.dist.foreign.service.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.copyright.rup.dist.common.test.TestUtils;
+import com.copyright.rup.dist.foreign.domain.Scenario;
+import com.copyright.rup.dist.foreign.domain.ScenarioStatusEnum;
+import com.copyright.rup.dist.foreign.domain.UsageDto;
+import com.copyright.rup.dist.foreign.service.api.IUsageService;
+import com.copyright.rup.dist.foreign.service.impl.mock.MockPaidUsageConsumer;
+
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.commons.collections4.CollectionUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Verifies counsuming paid information from LM and storing paid data to database.
+ * <p>
+ * Copyright (C) 2018 copyright.com
+ * <p>
+ * Date: 02/22/18
+ *
+ * @author Darya Baraukova
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+    value = {"classpath:/com/copyright/rup/dist/foreign/service/dist-foreign-service-test-context.xml"})
+@TestPropertySource(properties = {"test.liquibase.changelog=receive-paid-usages-from-lm-data-init.groovy"})
+public class ReceivePaidUsagesFromLmTest {
+
+    @Produce
+    private ProducerTemplate template;
+    @Autowired
+    @Qualifier("df.service.paidUsageConsumer")
+    private MockPaidUsageConsumer paidUsageConsumer;
+    @Autowired
+    private IUsageService usageService;
+
+    @Test
+    public void testReceivePaidUsagesFromLm() throws InterruptedException {
+        expectReceivePaidUsages();
+        List<UsageDto> usages =
+            usageService.getByScenarioAndRhAccountNumber(1000002859L, buildExpectedScenario(), null, null, null);
+        assertTrue(CollectionUtils.isNotEmpty(usages));
+        assertEquals(1, usages.size());
+    }
+
+    private void expectReceivePaidUsages() throws InterruptedException {
+        template.setDefaultEndpointUri("direct:topic:sf.processor.detail.paid");
+        CountDownLatch latch = new CountDownLatch(1);
+        paidUsageConsumer.setLatch(latch);
+        template.sendBodyAndHeader(TestUtils.fileToString(this.getClass(), "lm/paid_usages_fas.json"), "source", "FDA");
+        assertTrue(latch.await(10, TimeUnit.SECONDS));
+    }
+
+    private Scenario buildExpectedScenario() {
+        Scenario scenario = new Scenario();
+        scenario.setStatus(ScenarioStatusEnum.SENT_TO_LM);
+        scenario.setId("5b9ab240-17e3-11e8-b566-0800200c9a66");
+        return scenario;
+    }
+}
---
+++ dist-foreign-service/src/testInteg/resources/com/copyright/rup/dist/foreign/service/liquibase/receive-paid-usages-from-lm-data-init.groovy
@@ -0,0 +1,52 @@
+databaseChangeLog {
+    property(file: 'database.properties')
+
+    changeSet(id: '2018-02-22-00', author: 'Darya Baraukova <dbaraukova@copyright.com>') {
+        comment('Inserting test data to receive paid usages from LM')
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_scenario') {
+            column(name: 'df_scenario_uid', value: '5b9ab240-17e3-11e8-b566-0800200c9a66')
+            column(name: 'name', value: 'Paid Scenario')
+            column(name: 'status_ind', value: 'SENT_TO_LM')
+            column(name: 'description', value: 'Scenario already sent to LM')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage_batch') {
+            column(name: 'df_usage_batch_uid', value: '64a6ace0-17e3-11e8-b566-0800200c9a66')
+            column(name: 'name', value: 'Paid batch')
+            column(name: 'rro_account_number', value: '2000017010')
+            column(name: 'payment_date', value: '2021-02-12')
+            column(name: 'fiscal_year', value: '2020')
+            column(name: 'gross_amount', value: '1000.00')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage_archive') {
+            column(name: 'df_usage_archive_uid', value: '6a833980-17e3-11e8-b566-0800200c9a66')
+            column(name: 'df_usage_batch_uid', value: '64a6ace0-17e3-11e8-b566-0800200c9a66')
+            column(name: 'df_scenario_uid', value: '5b9ab240-17e3-11e8-b566-0800200c9a66')
+            column(name: 'detail_id', value: '5423214321')
+            column(name: 'product_family', value: 'FAS')
+            column(name: 'wr_wrk_inst', value: '243904752')
+            column(name: 'work_title', value: '100 ROAD MOVIES')
+            column(name: 'rh_account_number', value: '1000002859')
+            column(name: 'payee_account_number', value: '1000002859')
+            column(name: 'status_ind', value: 'LOCKED')
+            column(name: 'article', value: 'DIN EN 779:2012')
+            column(name: 'standard_number', value: '1008902112317555XX')
+            column(name: 'publisher', value: 'IEEE')
+            column(name: 'publication_date', value: '2013-09-10')
+            column(name: 'market', value: 'Doc Del')
+            column(name: 'market_period_from', value: '2013')
+            column(name: 'market_period_to', value: '2017')
+            column(name: 'author', value: 'Philippe de Mézières')
+            column(name: 'number_of_copies', value: '100')
+            column(name: 'reported_value', value: '6000')
+            column(name: 'gross_amount', value: '1000.00')
+            column(name: 'net_amount', value: '160.00')
+            column(name: 'service_fee_amount', value: '840.00')
+            column(name: 'service_fee', value: '0.16000')
+        }
+
+        rollback ""
+    }
+}
