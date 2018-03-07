8610856abfcec500117de1f22c98562611255cc5
Aliaksandr_Liakh <Aliaksandr_Liakh1@epam.com>
2018-03-07 08:35:28 -0500

    B-40635 FDA: Refresh in-progress scenario with newly eligible details: adding Liquibase scripts to store filters

--- dist-foreign-db/src/main/liquibase/com/copyright/rup/dist/foreign/db/changelog-2.groovy
+++ dist-foreign-db/src/main/liquibase/com/copyright/rup/dist/foreign/db/changelog-2.groovy
@@ -396,4 +396,131 @@ databaseChangeLog {
                     columnName: 'standard_number', columnDataType: 'VARCHAR(1000)')
         }
     }
+
+    changeSet(id: '2018-03-05-00', author: 'Aliaksandr Liakh <aliakh@copyright.com>') {
+        comment("B-40635 FDA: Refresh in-progress scenario with newly eligible details: adding Liquibase scripts to store filters")
+
+        createTable(tableName: 'df_usage_filter', schemaName: dbAppsSchema, tablespace: dbDataTablespace,
+                remarks: 'Table for storing usages filters') {
+
+            column(name: 'df_usage_filter_uid', type: 'VARCHAR(255)', remarks: 'The unique identifier for usage filter') {
+                constraints(nullable: false)
+            }
+            column(name: 'df_scenario_uid', type: 'VARCHAR(255)', remarks: 'The unique identifier of related scenario') {
+                constraints(nullable: false)
+            }
+            column(name: 'product_family', type: 'VARCHAR(255)', remarks: 'The product family') {
+                constraints(nullable: false)
+            }
+            column(name: 'status_ind', type: 'VARCHAR(16)', remarks: 'The status of usage') {
+                constraints(nullable: false)
+            }
+            column(name: 'payment_date', type: 'DATE', remarks: 'The payment date') {
+                constraints(nullable: false)
+            }
+            column(name: 'fiscal_year', type: 'NUMERIC(4,0)', remarks: 'The fiscal year') {
+                constraints(nullable: false)
+            }
+            column(name: 'record_version', type: 'INTEGER', defaultValue: '1', remarks: 'The latest version of this record, used for optimistic locking') {
+                constraints(nullable: false)
+            }
+            column(name: 'created_by_user', type: 'VARCHAR(256)', remarks: 'The user name who created this record') {
+                constraints(nullable: false)
+            }
+            column(name: 'created_datetime', type: 'TIMESTAMPTZ', defaultValueDate: 'now()', remarks: 'The date and time this record was created') {
+                constraints(nullable: false)
+            }
+            column(name: 'updated_by_user', type: 'VARCHAR(256)', remarks: 'The user name who updated this record; when a record is first created, this will be the same as the created_by_user') {
+                constraints(nullable: false)
+            }
+            column(name: 'updated_datetime', type: 'TIMESTAMPTZ', defaultValueDate: 'now()', remarks: 'The date and time this record was created; when a record is first created, this will be the same as the created_datetime') {
+                constraints(nullable: false)
+            }
+        }
+
+        addPrimaryKey(schemaName: dbAppsSchema, tableName: 'df_usage_filter', tablespace: dbIndexTablespace,
+                columnNames: 'df_usage_filter_uid', constraintName: 'pk_df_usage_filter_uid')
+
+        addForeignKeyConstraint(constraintName: 'fk_df_usage_filter_2_df_scenario',
+                baseTableSchemaName: dbAppsSchema, baseTableName: 'df_usage_filter', baseColumnNames: 'df_scenario_uid',
+                referencedTableSchemaName: dbAppsSchema, referencedTableName: 'df_scenario', referencedColumnNames: 'df_scenario_uid')
+
+        createTable(tableName: 'df_usage_filter_to_rh_account_numbers_map', schemaName: dbAppsSchema, tablespace: dbDataTablespace,
+                remarks: 'Table to map one to many relation between usage filter and rightsholder account numbers') {
+
+            column(name: 'df_usage_filter_uid', type: 'VARCHAR(255)', remarks: 'The unique identifier of usage filter') {
+                constraints(nullable: false)
+            }
+            column(name: 'rh_account_number', type: 'NUMERIC(22,0)', remarks: 'The rightsholder account number') {
+                constraints(nullable: false)
+            }
+            column(name: 'record_version', type: 'INTEGER', defaultValue: '1', remarks: 'The latest version of this record, used for optimistic locking') {
+                constraints(nullable: false)
+            }
+            column(name: 'created_by_user', type: 'VARCHAR(256)', remarks: 'The user name who created this record') {
+                constraints(nullable: false)
+            }
+            column(name: 'created_datetime', type: 'TIMESTAMPTZ', defaultValueDate: 'now()', remarks: 'The date and time this record was created') {
+                constraints(nullable: false)
+            }
+            column(name: 'updated_by_user', type: 'VARCHAR(256)', remarks: 'The user name who updated this record; when a record is first created, this will be the same as the created_by_user') {
+                constraints(nullable: false)
+            }
+            column(name: 'updated_datetime', type: 'TIMESTAMPTZ', defaultValueDate: 'now()', remarks: 'The date and time this record was created; when a record is first created, this will be the same as the created_datetime') {
+                constraints(nullable: false)
+            }
+        }
+
+        addPrimaryKey(tableName: 'df_usage_filter_to_rh_account_numbers_map', columnNames: 'df_usage_filter_uid, rh_account_number',
+                constraintName: 'pk_df_usage_filter_to_rh_account_numbers_map', schemaName: dbAppsSchema, tablespace: dbDataTablespace)
+
+        addForeignKeyConstraint(constraintName: 'fk_df_usage_filter_to_rh_account_numbers_map_2_df_usage_filter',
+                baseTableSchemaName: dbAppsSchema, baseTableName: 'df_usage_filter_to_rh_account_numbers_map', baseColumnNames: 'df_usage_filter_uid',
+                referencedTableSchemaName: dbAppsSchema, referencedTableName: 'df_usage_filter', referencedColumnNames: 'df_usage_filter_uid')
+        addForeignKeyConstraint(constraintName: 'fk_df_usage_filter_to_rh_account_numbers_map_2_df_rightsholder',
+                baseTableSchemaName: dbAppsSchema, baseTableName: 'df_usage_filter_to_rh_account_numbers_map', baseColumnNames: 'rh_account_number',
+                referencedTableSchemaName: dbAppsSchema, referencedTableName: 'df_rightsholder', referencedColumnNames: 'rh_account_number')
+
+        createTable(tableName: 'df_usage_filter_to_usage_batches_ids_map', schemaName: dbAppsSchema, tablespace: dbDataTablespace,
+                remarks: 'Table to map one to many relation between usage filter and usage batches ids') {
+
+            column(name: 'df_usage_filter_uid', type: 'VARCHAR(255)', remarks: 'The unique identifier of usage filter') {
+                constraints(nullable: false)
+            }
+            column(name: 'df_usage_batch_uid', type: 'VARCHAR(255)', remarks: 'The unique identifier of usage batch') {
+                constraints(nullable: false)
+            }
+            column(name: 'record_version', type: 'INTEGER', defaultValue: '1', remarks: 'The latest version of this record, used for optimistic locking') {
+                constraints(nullable: false)
+            }
+            column(name: 'created_by_user', type: 'VARCHAR(256)', remarks: 'The user name who created this record') {
+                constraints(nullable: false)
+            }
+            column(name: 'created_datetime', type: 'TIMESTAMPTZ', defaultValueDate: 'now()', remarks: 'The date and time this record was created') {
+                constraints(nullable: false)
+            }
+            column(name: 'updated_by_user', type: 'VARCHAR(256)', remarks: 'The user name who updated this record; when a record is first created, this will be the same as the created_by_user') {
+                constraints(nullable: false)
+            }
+            column(name: 'updated_datetime', type: 'TIMESTAMPTZ', defaultValueDate: 'now()', remarks: 'The date and time this record was created; when a record is first created, this will be the same as the created_datetime') {
+                constraints(nullable: false)
+            }
+        }
+
+        addPrimaryKey(tableName: 'df_usage_filter_to_usage_batches_ids_map', columnNames: 'df_usage_filter_uid, df_usage_batch_uid',
+                constraintName: 'pk_df_usage_filter_to_usage_batches_ids_map', schemaName: dbAppsSchema, tablespace: dbDataTablespace)
+
+        addForeignKeyConstraint(constraintName: 'fk_df_usage_filter_to_usage_batches_ids_map_2_df_usage_filter',
+                baseTableSchemaName: dbAppsSchema, baseTableName: 'df_usage_filter_to_usage_batches_ids_map', baseColumnNames: 'df_usage_filter_uid',
+                referencedTableSchemaName: dbAppsSchema, referencedTableName: 'df_usage_filter', referencedColumnNames: 'df_usage_filter_uid')
+        addForeignKeyConstraint(constraintName: 'fk_df_usage_filter_to_usage_batches_ids_map_2_df_usage_batch',
+                baseTableSchemaName: dbAppsSchema, baseTableName: 'df_usage_filter_to_usage_batches_ids_map', baseColumnNames: 'df_usage_batch_uid',
+                referencedTableSchemaName: dbAppsSchema, referencedTableName: 'df_usage_batch', referencedColumnNames: 'df_usage_batch_uid')
+
+        rollback {
+            dropTable(tableName: 'df_usage_filter_to_usage_batches_ids_map', schemaName: dbAppsSchema)
+            dropTable(tableName: 'df_usage_filter_to_rh_account_numbers_map', schemaName: dbAppsSchema)
+            dropTable(tableName: 'df_usage_filter', schemaName: dbAppsSchema)
+        }
+    }
 }
