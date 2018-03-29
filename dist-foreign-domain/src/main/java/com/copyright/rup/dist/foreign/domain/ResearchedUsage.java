ed4979884864bd8b51e44f8b68529e7b5b2e3609
Aliaksandr_Liakh <Aliaksandr_Liakh1@epam.com>
2018-03-29 09:22:29 -0400

    B-41583 FDA Reload researched details: implementing CSV file parsing

---
+++ dist-foreign-domain/src/main/java/com/copyright/rup/dist/foreign/domain/ResearchedUsage.java
@@ -0,0 +1,68 @@
+package com.copyright.rup.dist.foreign.domain;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * Represents researched usage detail.
+ * <p/>
+ * Copyright (C) 2018 copyright.com
+ * <p/>
+ * Date: 03/27/2018
+ *
+ * @author Aliaksandr Liakh
+ */
+public class ResearchedUsage {
+
+    private Long detailId;
+    private Long wrWrkInst;
+
+    public Long getDetailId() {
+        return detailId;
+    }
+
+    public void setDetailId(Long detailId) {
+        this.detailId = detailId;
+    }
+
+    public Long getWrWrkInst() {
+        return wrWrkInst;
+    }
+
+    public void setWrWrkInst(Long wrWrkInst) {
+        this.wrWrkInst = wrWrkInst;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (null == obj || this.getClass() != obj.getClass()) {
+            return false;
+        }
+        ResearchedUsage that = (ResearchedUsage) obj;
+        return new EqualsBuilder()
+            .append(this.detailId, that.detailId)
+            .append(this.wrWrkInst, that.wrWrkInst)
+            .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+            .append(detailId)
+            .append(wrWrkInst)
+            .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+            .append("detailId", detailId)
+            .append("wrWrkInst", wrWrkInst)
+            .toString();
+    }
+}
