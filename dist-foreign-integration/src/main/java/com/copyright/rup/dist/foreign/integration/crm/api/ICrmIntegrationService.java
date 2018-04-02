ff31f4a4bcf40365cfb726ca14c1cb7852b79e14
DaryaBaraukova <Darya_Baraukova@epam.com>
2018-04-02 10:47:16 +0300

    B-37304 FDA: reporting to RC (foundation): implement CrmIntegrationService to report paid information

---
+++ dist-foreign-integration/src/main/java/com/copyright/rup/dist/foreign/integration/crm/api/ICrmIntegrationService.java
@@ -0,0 +1,23 @@
+package com.copyright.rup.dist.foreign.integration.crm.api;
+
+import java.util.List;
+
+/**
+ * Interface for services to encapsulate logic for CRM system.
+ * <p>
+ * Copyright (C) 2018 copyright.com
+ * <p>
+ * Date: 04/02/18
+ *
+ * @author Darya Baraukova
+ */
+public interface ICrmIntegrationService {
+
+    /**
+     * Sends list of {@link CrmRightsDistributionRequest}s to the CRM system.
+     *
+     * @param crmRightsDistributionRequests list of {@link CrmRightsDistributionRequest} instances
+     * @return {@link CrmResult} instance
+     */
+    CrmResult sendRightsDistributionRequests(List<CrmRightsDistributionRequest> crmRightsDistributionRequests);
+}
