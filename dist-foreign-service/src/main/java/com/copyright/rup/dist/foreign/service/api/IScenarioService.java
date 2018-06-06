4a72e448512132256e8d584b6542dd5758aa8fc2
DaryaBaraukova <Darya_Baraukova@epam.com>
2018-06-06 17:19:57 +0300

    RDSC-597 FDA: Scenario status doesn't changed to ARCHIVED if details were sent to CRM in different time

--- dist-foreign-service/src/main/java/com/copyright/rup/dist/foreign/service/api/IScenarioService.java
+++ dist-foreign-service/src/main/java/com/copyright/rup/dist/foreign/service/api/IScenarioService.java
@@ -149,8 +149,7 @@ public interface IScenarioService {
      * Updates scenario status to {@link com.copyright.rup.dist.foreign.domain.ScenarioStatusEnum#ARCHIVED} if
      * all usages are in {@link com.copyright.rup.dist.foreign.domain.UsageStatusEnum#ARCHIVED} status.
      *
-     * @param paidUsageIds list of paid usages ids
      * @return count of archieved scenarios
      */
-    int archiveScenarios(List<String> paidUsageIds);
+    int archiveScenarios();
 }
