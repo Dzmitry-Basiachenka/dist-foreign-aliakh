198970683018747fdb9ace8da2842d38507663a6
PavelLiakh <Pavel_Liakh@epam.com>
2018-12-19 05:38:05 -0500

    B-48319 FDA/LM: Apply optimization for getting grants from RMS: add Liquibase changeset to create grant_priority table and populate it, write IT for GrantPriorityRepository

---
+++ dist-foreign-repository/src/testInteg/java/com/copyright/rup/dist/foreign/repository/impl/GrantPriorityRepositoryIntegrationTest.java
@@ -0,0 +1,93 @@
+package com.copyright.rup.dist.foreign.repository.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import com.copyright.rup.dist.common.domain.GrantPriority;
+import com.copyright.rup.dist.common.repository.api.IGrantPriorityRepository;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+/**
+ * Verifies {@link com.copyright.rup.dist.common.repository.impl.GrantPriorityRepository}.
+ * <p/>
+ * Copyright (C) 2017 copyright.com
+ * <p/>
+ * Date: 02/23/2017
+ *
+ * @author Pavel Liakh
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+    value = {"classpath:/com/copyright/rup/dist/foreign/repository/dist-foreign-repository-test-context.xml"})
+public class GrantPriorityRepositoryIntegrationTest {
+
+    private static final String INTERNAL_DISTRIBUTION = "INTERNAL";
+
+    @Autowired
+    private IGrantPriorityRepository grantPriorityRepository;
+
+    private String productFamily;
+
+    @Test
+    public void testFindByProductFamilyFas() {
+        this.productFamily = "FAS";
+        assertFindByProductFamily();
+    }
+
+    @Test
+    public void testFindByProductFamilyFas2() {
+        this.productFamily = "FAS2";
+        assertFindByProductFamily();
+    }
+
+    @Test
+    public void testFindByProductFamilyNts() {
+        this.productFamily = "NTS";
+        assertFindByProductFamily();
+    }
+
+    private void assertFindByProductFamily() {
+        List<GrantPriority> actualGrantPriorities = grantPriorityRepository.findByProductFamily(productFamily);
+        assertNotNull(actualGrantPriorities);
+        assertEquals(5, CollectionUtils.size(actualGrantPriorities));
+        List<GrantPriority> expectedGrantPriorities = Arrays.asList(
+            buildGrantPriority("CORPORATE", "EXTERNAL", "NGT_PHOTOCOPY", 0),
+            buildGrantPriority("CORPORATE", INTERNAL_DISTRIBUTION, "PRINT", 1),
+            buildGrantPriority("CORPORATE", INTERNAL_DISTRIBUTION, "DIGITAL", 2),
+            buildGrantPriority("ACADEMIC", INTERNAL_DISTRIBUTION, "PRINT", 3),
+            buildGrantPriority("ACADEMIC", INTERNAL_DISTRIBUTION, "DIGITAL", 4)
+        );
+        IntStream.of(0, 4)
+            .forEach(i -> assertGrantPriority(expectedGrantPriorities.get(i), actualGrantPriorities.get(i)));
+    }
+
+    private GrantPriority buildGrantPriority(String market, String distribution, String typeOfUse,
+                                             Integer priority) {
+        GrantPriority grantPriority = new GrantPriority();
+        grantPriority.setProductFamily(productFamily);
+        grantPriority.setMarket(market);
+        grantPriority.setDistribution(distribution);
+        grantPriority.setTypeOfUse(typeOfUse);
+        grantPriority.setPriority(priority);
+        return grantPriority;
+    }
+
+    private void assertGrantPriority(GrantPriority expected, GrantPriority actual) {
+        assertNotNull(actual.getId());
+        assertEquals(expected.getProductFamily(), actual.getProductFamily());
+        assertEquals(expected.getMarket(), actual.getMarket());
+        assertEquals(expected.getDistribution(), actual.getDistribution());
+        assertEquals(expected.getTypeOfUse(), actual.getTypeOfUse());
+        assertEquals(expected.getPriority(), actual.getPriority());
+    }
+}
