8a49899467e580e22836a3159e55931cdfe12fad
Aliaksandr_Liakh <Aliaksandr_Liakh1@epam.com>
2018-12-19 07:38:44 -0500

    B-47645 Tech Debt: FDA: Implement Liquibase script to add primary keys constraint to the column df_rightsholder_uid instead of the column rh_account_number

--- dist-foreign-db/src/main/liquibase/com/copyright/rup/dist/foreign/db/changelog-4.groovy
+++ dist-foreign-db/src/main/liquibase/com/copyright/rup/dist/foreign/db/changelog-4.groovy
@@ -33,4 +33,29 @@ databaseChangeLog {
                     columnName: 'fiscal_year', columnDataType: 'NUMERIC(4,0)')
         }
     }
+
+    changeSet(id: '2018-12-19-00', author: 'Aliaksandr Liakh <aliakh@copyright.com>') {
+        comment("B-47645 Tech Debt: FDA: Implement Liquibase script to add primary key constraint " +
+                "to the column df_rightsholder_uid instead of the column rh_account_number")
+
+        dropPrimaryKey(schemaName: dbAppsSchema, tableName: 'df_rightsholder',
+                constraintName: 'pk_rh_account_number')
+
+        dropUniqueConstraint(schemaName: dbAppsSchema, tableName: 'df_rightsholder',
+                columnNames: 'df_rightsholder_uid', constraintName: 'uk_df_rightsholder')
+
+        addPrimaryKey(tablespace: dbIndexTablespace, schemaName: dbAppsSchema, tableName: 'df_rightsholder',
+                columnNames: 'df_rightsholder_uid', constraintName: 'pk_df_rightsholder')
+
+        rollback {
+            dropPrimaryKey(schemaName: dbAppsSchema, tableName: 'df_rightsholder',
+                    constraintName: 'pk_df_rightsholder')
+
+            addUniqueConstraint(schemaName: dbAppsSchema, tableName: 'df_rightsholder',
+                    columnNames: 'df_rightsholder_uid', constraintName: 'uk_df_rightsholder')
+
+            addPrimaryKey(tablespace: dbIndexTablespace, schemaName: dbAppsSchema, tableName: 'df_rightsholder',
+                    columnNames: 'rh_account_number', constraintName: 'pk_rh_account_number')
+        }
+    }
 }
