dd342194122a8db41b178cfe154e123d7a84ee46
Uladzislau Shalamitski <Uladzislau_Shalamitski@epam.com>
2019-04-01 10:16:11 -0400

    B-50004 FDA: Add Standard Number Type to the usage for classification: implement validator to verify Wr Wrk Inst

---
+++ dist-foreign-service/src/main/java/com/copyright/rup/dist/foreign/service/impl/csv/validator/WrWrkInstValidator.java
@@ -0,0 +1,52 @@
+package com.copyright.rup.dist.foreign.service.impl.csv.validator;
+
+import com.copyright.rup.dist.common.service.impl.csv.DistCsvProcessor;
+import com.copyright.rup.dist.foreign.domain.Usage;
+import com.copyright.rup.dist.foreign.domain.Work;
+import com.copyright.rup.dist.foreign.integration.pi.api.IPiIntegrationService;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Objects;
+
+/**
+ * The validator to check if Wr Wrk Inst exists in PI.
+ * <p>
+ * Copyright (C) 2019 copyright.com
+ * <p>
+ * Date: 04/01/19
+ *
+ * @author Uladzislau Shalamitski
+ */
+public class WrWrkInstValidator implements DistCsvProcessor.IValidator<Usage> {
+
+    private final IPiIntegrationService piIntegrationService;
+
+    /**
+     * Constructor.
+     *
+     * @param piIntegrationService instance of {@link IPiIntegrationService}
+     */
+    public WrWrkInstValidator(IPiIntegrationService piIntegrationService) {
+        this.piIntegrationService = piIntegrationService;
+    }
+
+    @Override
+    public boolean isValid(Usage usage) {
+        boolean result = true;
+        if (Objects.nonNull(Objects.requireNonNull(usage).getWrWrkInst())) {
+            Work work = piIntegrationService.findWorkByWrWrkInst(usage.getWrWrkInst());
+            if (Objects.nonNull(work.getWrWrkInst())) {
+                usage.setStandardNumberType(StringUtils.upperCase(work.getMainIdnoType()));
+            } else {
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public String getErrorMessage() {
+        return "Loaded Wr Wrk Inst is missing in PI";
+    }
+}
