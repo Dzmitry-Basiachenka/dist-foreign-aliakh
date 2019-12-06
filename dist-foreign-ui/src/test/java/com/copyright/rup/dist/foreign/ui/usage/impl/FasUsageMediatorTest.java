5aeee3bd58e77197be8c9637e70ca5d325388f7d
Uladzislau Shalamitski <Uladzislau_Shalamitski@epam.com>
2019-12-06 03:28:15 -0500

    B-55626 FDA: Globalization of Product Family: implement FAS and NTS usages mediators

---
+++ dist-foreign-ui/src/test/java/com/copyright/rup/dist/foreign/ui/usage/impl/FasUsageMediatorTest.java
@@ -0,0 +1,115 @@
+package com.copyright.rup.dist.foreign.ui.usage.impl;
+
+import static org.easymock.EasyMock.anyString;
+import static org.easymock.EasyMock.expect;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.powermock.api.easymock.PowerMock.mockStatic;
+import static org.powermock.api.easymock.PowerMock.replay;
+import static org.powermock.api.easymock.PowerMock.verify;
+
+import com.copyright.rup.vaadin.security.SecurityUtils;
+
+import com.vaadin.ui.Button;
+import com.vaadin.ui.MenuBar;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ * Verifies {@link FasUsageMediator}.
+ * <p/>
+ * Copyright (C) 2019 copyright.com
+ * <p/>
+ * Date: 12/05/19
+ *
+ * @author Uladzislau Shalamitski
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(SecurityUtils.class)
+public class FasUsageMediatorTest {
+
+    private Button loadResearchedUsagesButton;
+    private Button addToScenarioButton;
+    private Button sendForResearchButton;
+    private MenuBar usageBatchMenuBar;
+    private MenuBar.MenuItem loadUsageBatchMenuItem;
+    private FasUsageMediator mediator;
+
+    @Before
+    public void setUp() {
+        loadResearchedUsagesButton = new Button();
+        addToScenarioButton = new Button();
+        sendForResearchButton = new Button();
+        usageBatchMenuBar = new MenuBar();
+        loadUsageBatchMenuItem = usageBatchMenuBar.new MenuItem(StringUtils.EMPTY, null, null);
+        mediator = new FasUsageMediator();
+        mediator.setLoadUsageBatchMenuItem(loadUsageBatchMenuItem);
+        mediator.setLoadResearchedUsagesButton(loadResearchedUsagesButton);
+        mediator.setAddToScenarioButton(addToScenarioButton);
+        mediator.setSendForResearchButton(sendForResearchButton);
+    }
+
+    @Test
+    public void testApplyPermissionsViewOnly() {
+        mockViewOnlyPermissions();
+        replay(SecurityUtils.class);
+        mediator.applyPermissions();
+        assertTrue(usageBatchMenuBar.isVisible());
+        assertFalse(loadUsageBatchMenuItem.isVisible());
+        assertFalse(loadResearchedUsagesButton.isVisible());
+        assertFalse(addToScenarioButton.isVisible());
+        assertFalse(sendForResearchButton.isVisible());
+        verify(SecurityUtils.class);
+    }
+
+    @Test
+    public void testApplyPermissionsDistributionManager() {
+        mockManagerPermissions();
+        replay(SecurityUtils.class);
+        mediator.applyPermissions();
+        assertTrue(usageBatchMenuBar.isVisible());
+        assertFalse(loadUsageBatchMenuItem.isVisible());
+        assertFalse(loadResearchedUsagesButton.isVisible());
+        assertFalse(addToScenarioButton.isVisible());
+        assertFalse(sendForResearchButton.isVisible());
+        verify(SecurityUtils.class);
+    }
+
+    @Test
+    public void testApplyPermissionsDistributionSpecialist() {
+        mockSpecialistPermissions();
+        replay(SecurityUtils.class);
+        mediator.applyPermissions();
+        assertTrue(usageBatchMenuBar.isVisible());
+        assertTrue(loadUsageBatchMenuItem.isVisible());
+        assertTrue(loadResearchedUsagesButton.isVisible());
+        assertTrue(addToScenarioButton.isVisible());
+        assertTrue(sendForResearchButton.isVisible());
+        verify(SecurityUtils.class);
+    }
+
+    private void mockViewOnlyPermissions() {
+        mockStatic(SecurityUtils.class);
+        expect(SecurityUtils.hasPermission(anyString())).andStubReturn(false);
+        expect(SecurityUtils.hasPermission("FDA_ACCESS_APPLICATION")).andReturn(true).anyTimes();
+    }
+
+    private void mockManagerPermissions() {
+        mockStatic(SecurityUtils.class);
+        expect(SecurityUtils.hasPermission(anyString())).andStubReturn(false);
+        expect(SecurityUtils.hasPermission("FDA_ACCESS_APPLICATION")).andReturn(true).anyTimes();
+    }
+
+    private void mockSpecialistPermissions() {
+        mockStatic(SecurityUtils.class);
+        expect(SecurityUtils.hasPermission("FDA_ACCESS_APPLICATION")).andReturn(true).anyTimes();
+        expect(SecurityUtils.hasPermission("FDA_LOAD_USAGE")).andReturn(true).anyTimes();
+        expect(SecurityUtils.hasPermission("FDA_LOAD_RESEARCHED_USAGE")).andReturn(true).anyTimes();
+        expect(SecurityUtils.hasPermission("FDA_CREATE_EDIT_SCENARIO")).andReturn(true).anyTimes();
+        expect(SecurityUtils.hasPermission("FDA_SEND_FOR_WORK_RESEARCH")).andReturn(true).anyTimes();
+    }
+}
