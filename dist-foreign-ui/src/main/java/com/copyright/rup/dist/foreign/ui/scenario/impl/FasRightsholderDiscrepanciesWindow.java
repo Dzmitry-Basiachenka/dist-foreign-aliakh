1e16552b49e5310f92a3a06619535dffbe031966
StanislauRudak <stanislau_rudak@epam.com>
2019-12-13 02:16:31 -0500

    B-55626 FDA: Globalization of Product Family: Add FAS Scenarios widget and controller

---
+++ dist-foreign-ui/src/main/java/com/copyright/rup/dist/foreign/ui/scenario/impl/FasRightsholderDiscrepanciesWindow.java
@@ -0,0 +1,133 @@
+package com.copyright.rup.dist.foreign.ui.scenario.impl;
+
+import com.copyright.rup.dist.common.reporting.impl.CsvStreamSource;
+import com.copyright.rup.dist.foreign.domain.RightsholderDiscrepancy;
+import com.copyright.rup.dist.foreign.ui.main.ForeignUi;
+import com.copyright.rup.dist.foreign.ui.scenario.api.IFasScenariosController;
+import com.copyright.rup.dist.foreign.ui.scenario.api.IReconcileRightsholdersController;
+import com.copyright.rup.vaadin.ui.Buttons;
+import com.copyright.rup.vaadin.ui.component.dataprovider.LoadingIndicatorDataProvider;
+import com.copyright.rup.vaadin.ui.component.downloader.OnDemandFileDownloader;
+import com.copyright.rup.vaadin.ui.component.window.Windows;
+import com.copyright.rup.vaadin.util.VaadinUtils;
+
+import com.vaadin.data.provider.DataProvider;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.Grid.SelectionMode;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.List;
+
+/**
+ * Represents component to display rightsholder discrepancies.
+ * <p>
+ * Copyright (C) 2018 copyright.com
+ * <p>
+ * Date: 01/25/18
+ *
+ * @author Ihar Suvorau
+ */
+// TODO {srudak} remove when  migration to new UI classes is finished; adjust original RightsholderDiscrepanciesWindow
+class FasRightsholderDiscrepanciesWindow extends Window {
+
+    private final IReconcileRightsholdersController controller;
+    private final IFasScenariosController scenariosController;
+    private Grid<RightsholderDiscrepancy> grid;
+
+    /**
+     * Constructor.
+     *
+     * @param reconcileRightsholdersController instance of {@link IReconcileRightsholdersController}
+     * @param scenariosController              instance of {@link IFasScenariosController}
+     */
+    FasRightsholderDiscrepanciesWindow(IReconcileRightsholdersController reconcileRightsholdersController,
+                                       IFasScenariosController scenariosController) {
+        this.controller = reconcileRightsholdersController;
+        this.scenariosController = scenariosController;
+        setWidth(900, Unit.PIXELS);
+        setHeight(530, Unit.PIXELS);
+        setContent(initContent());
+        setCaption(ForeignUi.getMessage("label.reconcile_rightsholders"));
+        VaadinUtils.addComponentStyle(this, "rightsholder-discrepancies-window");
+    }
+
+    private Component initContent() {
+        initGrid();
+        HorizontalLayout buttons = initButtons();
+        VerticalLayout content = new VerticalLayout(grid, buttons);
+        content.setSizeFull();
+        content.setExpandRatio(grid, 1.0f);
+        content.setComponentAlignment(buttons, Alignment.BOTTOM_RIGHT);
+        return content;
+    }
+
+    private HorizontalLayout initButtons() {
+        HorizontalLayout buttonsLayout = new HorizontalLayout();
+        Button exportButton = Buttons.createButton(ForeignUi.getMessage("button.export"));
+        OnDemandFileDownloader fileDownloader = new OnDemandFileDownloader(new CsvStreamSource(controller).getSource());
+        fileDownloader.extend(exportButton);
+        Button approveButton = Buttons.createButton(ForeignUi.getMessage("button.approve"));
+        approveButton.addClickListener(event -> {
+            List<Long> prohibitedAccountNumbers = controller.getProhibitedAccountNumbers();
+            if (CollectionUtils.isEmpty(prohibitedAccountNumbers)) {
+                Windows.showConfirmDialog(
+                    ForeignUi.getMessage("message.confirm.approve", controller.getScenario().getName()), () -> {
+                        controller.approveReconciliation();
+                        this.close();
+                        scenariosController.getWidget().refreshSelectedScenario();
+                    });
+            } else {
+                Windows.showNotificationWindow(
+                    ForeignUi.getMessage("window.prohibition_approval", prohibitedAccountNumbers));
+            }
+        });
+        buttonsLayout.addComponents(exportButton, approveButton, Buttons.createCancelButton(this));
+        buttonsLayout.setSpacing(true);
+        VaadinUtils.addComponentStyle(buttonsLayout, "rightsholder-discrepancies-buttons-layout");
+        return buttonsLayout;
+    }
+
+    private void initGrid() {
+        DataProvider<RightsholderDiscrepancy, Void> dataProvider = LoadingIndicatorDataProvider.fromCallbacks(
+            query -> controller.loadBeans(query.getOffset(), query.getLimit(), query.getSortOrders()).stream(),
+            query -> controller.getBeansCount()
+        );
+        grid = new Grid<>(dataProvider);
+        addColumns();
+        grid.setSizeFull();
+        grid.setSelectionMode(SelectionMode.NONE);
+        grid.getColumns().forEach(column -> column.setSortable(true));
+        VaadinUtils.addComponentStyle(grid, "rightsholder-discrepancies-grid");
+    }
+
+    private void addColumns() {
+        grid.addColumn(discrepancy -> discrepancy.getOldRightsholder().getAccountNumber())
+            .setCaption(ForeignUi.getMessage("table.column.rh_account_number"))
+            .setSortProperty("oldRightsholder.rhAccountNumber")
+            .setWidth(115);
+        grid.addColumn(discrepancy -> discrepancy.getOldRightsholder().getName())
+            .setCaption(ForeignUi.getMessage("table.column.rh_account_name"))
+            .setSortProperty("oldRightsholder.rhName");
+        grid.addColumn(discrepancy -> discrepancy.getNewRightsholder().getAccountNumber())
+            .setCaption(ForeignUi.getMessage("table.column.new_rh_account_number"))
+            .setSortProperty("newRightsholder.rhAccountNumber")
+            .setWidth(140);
+        grid.addColumn(discrepancy -> discrepancy.getNewRightsholder().getName())
+            .setCaption(ForeignUi.getMessage("table.column.new_rh_name"))
+            .setSortProperty("newRightsholder.rhName");
+        grid.addColumn(RightsholderDiscrepancy::getWrWrkInst)
+            .setCaption(ForeignUi.getMessage("table.column.wr_wrk_inst"))
+            .setSortProperty("wrWrkInst")
+            .setWidth(110);
+        grid.addColumn(RightsholderDiscrepancy::getWorkTitle)
+            .setCaption(ForeignUi.getMessage("table.column.work_title"))
+            .setSortProperty("workTitle");
+    }
+}
---
+++ dist-foreign-ui/src/main/java/com/copyright/rup/dist/foreign/ui/scenario/impl/FasScenariosWidget.java
@@ -0,0 +1,136 @@
+package com.copyright.rup.dist.foreign.ui.scenario.impl;
+
+import com.copyright.rup.dist.foreign.domain.Scenario;
+import com.copyright.rup.dist.foreign.domain.ScenarioActionTypeEnum;
+import com.copyright.rup.dist.foreign.ui.main.ForeignUi;
+import com.copyright.rup.dist.foreign.ui.scenario.api.IFasScenariosController;
+import com.copyright.rup.dist.foreign.ui.scenario.api.IFasScenariosWidget;
+import com.copyright.rup.dist.foreign.ui.scenario.api.IScenarioHistoryController;
+import com.copyright.rup.dist.foreign.ui.scenario.api.IScenariosMediator;
+import com.copyright.rup.vaadin.ui.Buttons;
+import com.copyright.rup.vaadin.util.CurrencyUtils;
+import com.copyright.rup.vaadin.util.VaadinUtils;
+import com.copyright.rup.vaadin.widget.api.IMediator;
+
+import com.vaadin.shared.ui.ContentMode;
+import com.vaadin.shared.ui.MarginInfo;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.math.BigDecimal;
+
+/**
+ * Implementation of {@link IFasScenariosWidget}.
+ * <p>
+ * Copyright (C) 2019 copyright.com
+ * <p>
+ * Date: 12/11/19
+ *
+ * @author Stanislau Rudak
+ */
+public class FasScenariosWidget extends CommonScenariosWidget<IFasScenariosWidget, IFasScenariosController>
+    implements IFasScenariosWidget {
+
+    private final Button deleteButton = Buttons.createButton(ForeignUi.getMessage("button.delete"));
+    private final Button viewButton = Buttons.createButton(ForeignUi.getMessage("button.view"));
+    private final Button submitButton = Buttons.createButton(ForeignUi.getMessage("button.submit"));
+    private final Button rejectButton = Buttons.createButton(ForeignUi.getMessage("button.reject"));
+    private final Button approveButton = Buttons.createButton(ForeignUi.getMessage("button.approve"));
+    private final Button sendToLmButton = Buttons.createButton(ForeignUi.getMessage("button.send_to_lm"));
+    private final Button reconcileRightsholdersButton =
+        Buttons.createButton(ForeignUi.getMessage("button.reconcile_rightsholders"));
+    private final Button refreshScenarioButton = Buttons.createButton(ForeignUi.getMessage("button.refresh_scenario"));
+    private final Label ownerLabel = new Label(StringUtils.EMPTY, ContentMode.HTML);
+    private final Label netTotalLabel = new Label(StringUtils.EMPTY, ContentMode.HTML);
+    private final Label reportedTotalLabel = new Label(StringUtils.EMPTY, ContentMode.HTML);
+    private final Label grossTotalLabel = new Label(StringUtils.EMPTY, ContentMode.HTML);
+    private final Label descriptionLabel = new Label(StringUtils.EMPTY, ContentMode.HTML);
+    private final Label selectionCriteriaLabel = new Label(StringUtils.EMPTY, ContentMode.HTML);
+    private FasScenariosMediator mediator;
+
+    /**
+     * Controller.
+     *
+     * @param historyController instance of {@link IScenarioHistoryController}
+     */
+    FasScenariosWidget(IScenarioHistoryController historyController) {
+        super(historyController);
+    }
+
+    @Override
+    public IMediator initMediator() {
+        mediator = new FasScenariosMediator();
+        mediator.setViewButton(viewButton);
+        mediator.setDeleteButton(deleteButton);
+        mediator.setApproveButton(approveButton);
+        mediator.setRejectButton(rejectButton);
+        mediator.setSubmitButton(submitButton);
+        mediator.setSendToLmButton(sendToLmButton);
+        mediator.setReconcileRightsholdersButton(reconcileRightsholdersButton);
+        mediator.setRefreshScenarioButton(refreshScenarioButton);
+        mediator.selectedScenarioChanged(getSelectedScenario());
+        return mediator;
+    }
+
+    @Override
+    protected HorizontalLayout initButtonsLayout() {
+        HorizontalLayout layout = new HorizontalLayout();
+        addButtonsListeners();
+        VaadinUtils.setButtonsAutoDisabled(viewButton, deleteButton, reconcileRightsholdersButton, submitButton,
+            rejectButton, approveButton, sendToLmButton, refreshScenarioButton);
+        layout.addComponents(viewButton, deleteButton, reconcileRightsholdersButton, submitButton, rejectButton,
+            approveButton, sendToLmButton, refreshScenarioButton);
+        layout.setMargin(true);
+        VaadinUtils.addComponentStyle(layout, "scenarios-buttons");
+        return layout;
+    }
+
+    private void addButtonsListeners() {
+        deleteButton.addClickListener(event -> getController().onDeleteButtonClicked());
+        viewButton.addClickListener(event -> getController().onViewButtonClicked());
+        reconcileRightsholdersButton.addClickListener(event -> getController().onReconcileRightsholdersButtonClicked());
+        submitButton.addClickListener(event -> getController().handleAction(ScenarioActionTypeEnum.SUBMITTED));
+        rejectButton.addClickListener(event -> getController().handleAction(ScenarioActionTypeEnum.REJECTED));
+        approveButton.addClickListener(event -> getController().handleAction(ScenarioActionTypeEnum.APPROVED));
+        sendToLmButton.addClickListener(event -> getController().sendToLm());
+        refreshScenarioButton.addClickListener(event -> getController().onRefreshScenarioButtonClicked());
+    }
+
+    @Override
+    protected VerticalLayout initMetadataLayout() {
+        descriptionLabel.setStyleName("v-label-white-space-normal");
+        selectionCriteriaLabel.setStyleName("v-label-white-space-normal");
+        VerticalLayout metadataLayout =
+            new VerticalLayout(ownerLabel, netTotalLabel, grossTotalLabel, reportedTotalLabel,
+                descriptionLabel, selectionCriteriaLabel);
+        metadataLayout.setMargin(new MarginInfo(false, true, false, true));
+        VaadinUtils.setMaxComponentsWidth(metadataLayout);
+        return metadataLayout;
+    }
+
+    @Override
+    protected void updateScenarioMetadata(Scenario scenarioWithAmounts) {
+        ownerLabel.setValue(ForeignUi.getMessage("label.owner", scenarioWithAmounts.getCreateUser()));
+        netTotalLabel.setValue(ForeignUi.getMessage("label.net_amount_in_usd",
+            formatAmount(scenarioWithAmounts.getNetTotal())));
+        grossTotalLabel.setValue(ForeignUi.getMessage("label.gross_amount_in_usd",
+            formatAmount(scenarioWithAmounts.getGrossTotal())));
+        reportedTotalLabel.setValue(ForeignUi.getMessage("label.reported_total",
+            formatAmount(scenarioWithAmounts.getReportedTotal())));
+        descriptionLabel.setValue(ForeignUi.getMessage("label.description", scenarioWithAmounts.getDescription()));
+        selectionCriteriaLabel.setValue(getController().getCriteriaHtmlRepresentation());
+    }
+
+    @Override
+    protected IScenariosMediator getMediator() {
+        return mediator;
+    }
+
+    private String formatAmount(BigDecimal amount) {
+        return CurrencyUtils.formatAsHtml(amount.setScale(2, BigDecimal.ROUND_HALF_UP));
+    }
+}
