35ea9b8e1ab3a691eb8d1aee82639e5a02f9477c
IharSurovau1 <Ihar_Suvorau@epam.com>
2019-12-27 02:30:49 -0500

    B-55412 FDA: Load AACL Usage Data: implement logic for inserting batch with usages in database

--- dist-foreign-ui/src/main/java/com/copyright/rup/dist/foreign/ui/usage/api/aacl/IAaclUsageController.java
+++ dist-foreign-ui/src/main/java/com/copyright/rup/dist/foreign/ui/usage/api/aacl/IAaclUsageController.java
@@ -1,6 +1,13 @@
 package com.copyright.rup.dist.foreign.ui.usage.api.aacl;
 
+import com.copyright.rup.dist.common.reporting.api.IStreamSource;
+import com.copyright.rup.dist.common.service.impl.csv.DistCsvProcessor.ProcessingResult;
+import com.copyright.rup.dist.foreign.domain.Usage;
+import com.copyright.rup.dist.foreign.domain.UsageBatch;
+import com.copyright.rup.dist.foreign.service.impl.csv.AaclUsageCsvProcessor;
 import com.copyright.rup.dist.foreign.ui.usage.api.ICommonUsageController;
+
+import java.util.Collection;
 
 /**
  * Interface for AACL usages controller.
@@ -12,4 +19,29 @@ import com.copyright.rup.dist.foreign.ui.usage.api.ICommonUsageController;
  * @author Aliaksandr Liakh
  */
 public interface IAaclUsageController extends ICommonUsageController {
+
+    /**
+     * Inserts usage batch and it's usages.
+     *
+     * @param usageBatch {@link UsageBatch} instance
+     * @param usages     list of {@link Usage}s
+     * @return count of inserted usages
+     */
+    int loadUsageBatch(UsageBatch usageBatch, Collection<Usage> usages);
+
+    /**
+     * Gets instance of CSV processor.
+     *
+     * @return instance of {@link AaclUsageCsvProcessor}
+     */
+    AaclUsageCsvProcessor getCsvProcessor();
+
+    /**
+     * Return instance of {@link IStreamSource} for errors result.
+     *
+     * @param fileName         name of processed file
+     * @param processingResult information about errors
+     * @return instance of {@link IStreamSource}
+     */
+    IStreamSource getErrorResultStreamSource(String fileName, ProcessingResult processingResult);
 }
