54539e03ea16a7f61db7a6a3e3f63069112bd6ba
IharSurovau1 <Ihar_Suvorau@epam.com>
2020-01-21 07:20:09 -0500

    B-56092 Tech Debt: FDA: introduce separate repository for AACL product family

---
+++ dist-foreign-repository/src/testInteg/java/com/copyright/rup/dist/foreign/repository/impl/AaclUsageRepositoryIntegrationTest.java
@@ -0,0 +1,102 @@
+package com.copyright.rup.dist.foreign.repository.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import com.copyright.rup.dist.common.test.TestUtils;
+import com.copyright.rup.dist.foreign.domain.AaclUsage;
+import com.copyright.rup.dist.foreign.domain.Usage;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+/**
+ * Integration test for {@link AaclUsageRepository}.
+ * <p>
+ * Copyright (C) 2020 copyright.com
+ * <p>
+ * Date: 01/21/2020
+ *
+ * @author Ihar Suvorau
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+    value = {"classpath:/com/copyright/rup/dist/foreign/repository/dist-foreign-repository-test-context.xml"})
+@TestPropertySource(properties = {"test.liquibase.changelog=aacl-usage-repository-test-data-init.groovy"})
+@Rollback
+@Transactional
+public class AaclUsageRepositoryIntegrationTest {
+
+    private static final String USAGE_ID_1 = "6c91f04e-60dc-49e0-9cdc-e782e0b923e2";
+    private static final String USAGE_ID_2 = "0b5ac9fc-63e2-4162-8d63-953b7023293c";
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    static {
+        OBJECT_MAPPER.registerModule(new JavaTimeModule());
+        OBJECT_MAPPER.disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE);
+    }
+
+    @Autowired
+    private AaclUsageRepository aaclUsageRepository;
+
+    @Test
+    public void testFindByIds() throws IOException {
+        List<Usage> actualUsages = aaclUsageRepository.findByIds(Arrays.asList(USAGE_ID_1, USAGE_ID_2));
+        assertEquals(2, CollectionUtils.size(actualUsages));
+        verifyUsages(loadExpectedUsages("json/aacl/aacl_usages.json"), actualUsages);
+    }
+
+    private void verifyUsages(List<Usage> expectedUsages, List<Usage> actualUsages) {
+        assertEquals(CollectionUtils.size(expectedUsages), CollectionUtils.size(actualUsages));
+        IntStream.range(0, expectedUsages.size())
+            .forEach(index -> verifyUsage(expectedUsages.get(index), actualUsages.get(index)));
+    }
+
+    private void verifyUsage(Usage expectedUsage, Usage actualUsage) {
+        assertEquals(expectedUsage.getId(), actualUsage.getId());
+        assertEquals(expectedUsage.getBatchId(), actualUsage.getBatchId());
+        assertEquals(expectedUsage.getWrWrkInst(), actualUsage.getWrWrkInst());
+        assertEquals(expectedUsage.getProductFamily(), actualUsage.getProductFamily());
+        assertEquals(expectedUsage.getStandardNumber(), actualUsage.getStandardNumber());
+        assertEquals(expectedUsage.getStandardNumberType(), actualUsage.getStandardNumberType());
+        assertEquals(expectedUsage.getRightsholder().getName(), actualUsage.getRightsholder().getName());
+        assertEquals(expectedUsage.getRightsholder().getId(), actualUsage.getRightsholder().getId());
+        assertEquals(expectedUsage.getRightsholder().getAccountNumber(),
+            actualUsage.getRightsholder().getAccountNumber());
+        assertEquals(expectedUsage.getComment(), actualUsage.getComment());
+        AaclUsage expectedAaclUsage = expectedUsage.getAaclUsage();
+        AaclUsage actualAaclUsage = actualUsage.getAaclUsage();
+        assertEquals(expectedAaclUsage.getPublicationType(), actualAaclUsage.getPublicationType());
+        assertEquals(expectedAaclUsage.getRightLimitation(), actualAaclUsage.getRightLimitation());
+        assertEquals(expectedAaclUsage.getInstitution(), actualAaclUsage.getInstitution());
+        assertEquals(expectedAaclUsage.getNumberOfPages(), actualAaclUsage.getNumberOfPages());
+        assertEquals(expectedAaclUsage.getEnrollmentProfile(), actualAaclUsage.getEnrollmentProfile());
+        assertEquals(expectedAaclUsage.getUsagePeriod(), actualAaclUsage.getUsagePeriod());
+        assertEquals(expectedAaclUsage.getUsageSource(), actualAaclUsage.getUsageSource());
+        assertEquals(expectedAaclUsage.getDiscipline(), actualAaclUsage.getDiscipline());
+        assertEquals(expectedAaclUsage.getBatchPeriodEndDate(), actualAaclUsage.getBatchPeriodEndDate());
+    }
+
+    private List<Usage> loadExpectedUsages(String fileName) throws IOException {
+        String content = TestUtils.fileToString(this.getClass(), fileName);
+        return OBJECT_MAPPER.readValue(content, new TypeReference<List<Usage>>() {
+        });
+    }
+}
