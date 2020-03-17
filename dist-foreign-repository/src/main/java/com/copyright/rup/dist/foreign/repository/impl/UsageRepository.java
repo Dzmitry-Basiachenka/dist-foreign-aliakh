86abceedb2aaea076fafaf64db5f54c35485b5af
antonAzarenka <antonAzarenka@github.com>
2020-03-17 12:27:30 +0300

    B-27613 FDA: Audit Tab modifications for AACL: fix checkstyle issue

--- dist-foreign-repository/src/main/java/com/copyright/rup/dist/foreign/repository/impl/UsageRepository.java
+++ dist-foreign-repository/src/main/java/com/copyright/rup/dist/foreign/repository/impl/UsageRepository.java
@@ -27,7 +27,6 @@ import org.springframework.stereotype.Repository;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
