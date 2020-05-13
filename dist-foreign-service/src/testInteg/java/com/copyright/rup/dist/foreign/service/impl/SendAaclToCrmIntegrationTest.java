44c472468a200ad4ed5324fef9b50bdb4ef8e423
antonAzarenka <antonAzarenka@github.com>
2020-05-13 16:12:43 +0300

    B-56615 FDA: Reporting AACL to RC: adjust integration test to send report to crm

---
+++ dist-foreign-service/src/testInteg/java/com/copyright/rup/dist/foreign/service/impl/SendAaclToCrmIntegrationTest.java
@@ -0,0 +1,89 @@
+package com.copyright.rup.dist.foreign.service.impl;
+
+import com.copyright.rup.dist.common.domain.job.JobInfo;
+import com.copyright.rup.dist.common.domain.job.JobStatusEnum;
+import com.copyright.rup.dist.foreign.domain.ScenarioActionTypeEnum;
+import com.copyright.rup.dist.foreign.domain.ScenarioAuditItem;
+import com.copyright.rup.dist.foreign.domain.ScenarioStatusEnum;
+import com.copyright.rup.dist.foreign.domain.UsageActionTypeEnum;
+import com.copyright.rup.dist.foreign.domain.UsageAuditItem;
+import com.copyright.rup.dist.foreign.domain.UsageStatusEnum;
+
+import com.google.common.collect.ImmutableMap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Verifies logic for sending AACL {@link com.copyright.rup.dist.foreign.domain.PaidUsage}s to CRM.
+ * <p>
+ * Copyright (C) 2020 copyright.com
+ * <p>
+ * Date: 05/13/20
+ *
+ * @author Anton Azarenka
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+    value = {"classpath:/com/copyright/rup/dist/foreign/service/dist-foreign-service-test-context.xml"})
+@TestPropertySource(properties = {"test.liquibase.changelog=send-aacl-to-crm-data-init.groovy"})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public class SendAaclToCrmIntegrationTest {
+
+    @Autowired
+    private SendToCrmIntegrationTestBuilder testBuilder;
+
+    @Test
+    public void testSendToCrm() {
+        testBuilder
+            .withProductFamily("AACL")
+            .expectCrmCall("crm/sendToCrm/rights_distribution_request_aacl.json",
+                "crm/sendToCrm/rights_distribution_response_aacl.json")
+            .expectJobInfo(buildJobInfo())
+            .expectUsageStatus(ImmutableMap.of(
+                "8ab89fcc-abf9-432e-b653-e84f2605697f", UsageStatusEnum.ARCHIVED,
+                "e5ae9237-05a0-4c82-b607-0f91f19b2f24", UsageStatusEnum.ARCHIVED))
+            .expectScenarioStatus(ImmutableMap.of(
+                "351e585c-0b08-429d-9e31-bea283ba33de", ScenarioStatusEnum.ARCHIVED))
+            .expectUsageAudit(ImmutableMap.of(
+                "8ab89fcc-abf9-432e-b653-e84f2605697f", buildArchivedUsageAudit(),
+                "e5ae9237-05a0-4c82-b607-0f91f19b2f24", buildArchivedUsageAudit()))
+            .expectUsageAudit(ImmutableMap.of(
+                "8ab89fcc-abf9-432e-b653-e84f2605697f", buildArchivedUsageAudit(),
+                "e5ae9237-05a0-4c82-b607-0f91f19b2f24", buildArchivedUsageAudit()))
+            .expectScenarioAudit(ImmutableMap.of(
+                "351e585c-0b08-429d-9e31-bea283ba33de", buildArchivedScenarioAudit()))
+            .build()
+            .run();
+    }
+
+    private JobInfo buildJobInfo() {
+        JobInfo jobInfo = new JobInfo();
+        jobInfo.setStatus(JobStatusEnum.FINISHED);
+        jobInfo.setResult(
+            "PaidUsagesCount=2, ArchivedUsagesCount=2, NotReportedUsagesCount=0, ArchivedScenariosCount=1");
+        return jobInfo;
+    }
+
+    private List<UsageAuditItem> buildArchivedUsageAudit() {
+        UsageAuditItem auditItem = new UsageAuditItem();
+        auditItem.setActionType(UsageActionTypeEnum.ARCHIVED);
+        auditItem.setActionReason("Usage was sent to CRM");
+        return Collections.singletonList(auditItem);
+    }
+
+    private List<ScenarioAuditItem> buildArchivedScenarioAudit() {
+        ScenarioAuditItem auditItem = new ScenarioAuditItem();
+        auditItem.setActionType(ScenarioActionTypeEnum.ARCHIVED);
+        auditItem.setActionReason("All usages from scenario have been sent to CRM");
+        return Collections.singletonList(auditItem);
+    }
+}
---
+++ dist-foreign-service/src/testInteg/resources/com/copyright/rup/dist/foreign/service/liquibase/send-aacl-to-crm-data-init.groovy
@@ -0,0 +1,147 @@
+databaseChangeLog {
+    property(file: 'database.properties')
+
+    changeSet(id: '2020-05-13-00', author: 'Anton Azarenka <aazarenka@copyright.com>') {
+        comment('Inserting test data for testSendToCrm')
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_rightsholder') {
+            column(name: 'df_rightsholder_uid', value: '0b334c49-ff42-4768-b338-0363519601d9')
+            column(name: 'rh_account_number', value: '1000002859')
+            column(name: 'name', value: 'John Wiley & Sons - Books')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_rightsholder') {
+            column(name: 'df_rightsholder_uid', value: 'd2e6d7a1-86ac-435b-84bf-e421da550a3b')
+            column(name: 'rh_account_number', value: '1000005413')
+            column(name: 'name', value: 'Kluwer Academic Publishers - Dordrecht')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_scenario') {
+            column(name: 'df_scenario_uid', value: '351e585c-0b08-429d-9e31-bea283ba33de')
+            column(name: 'name', value: 'Paid Scenario')
+            column(name: 'status_ind', value: 'SENT_TO_LM')
+            column(name: 'description', value: 'Scenario already sent to LM')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_scenario_usage_filter') {
+            column(name: 'df_scenario_usage_filter_uid', value: '4109ca95-326d-428a-9d7e-1a90bc891dba')
+            column(name: 'df_scenario_uid', value: '351e585c-0b08-429d-9e31-bea283ba33de')
+            column(name: 'product_family', value: 'AACL')
+            column(name: 'status_ind', value: 'ELIGIBLE')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage_batch') {
+            column(name: 'df_usage_batch_uid', value: 'a87218a0-1966-40aa-846a-434b3fd41282')
+            column(name: 'name', value: 'Paid batch')
+            column(name: 'payment_date', value: '2020-05-24')
+            column(name: 'product_family', value: 'AACL')
+            column(name: 'gross_amount', value: '1000.00')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage_baseline_aacl') {
+            column(name: 'df_usage_baseline_aacl_uid', value: '9aeb87ed-5000-4bee-abcb-e3dfc4f84235')
+            column(name: 'wr_wrk_inst', value: '123456789')
+            column(name: 'usage_period', value: '2016')
+            column(name: 'usage_source', value: 'Aug 2016 FR')
+            column(name: 'number_of_copies', value: '30')
+            column(name: 'number_of_pages', value: '35')
+            column(name: 'detail_licensee_class_id', value: '143')
+            column(name: 'original_publication_type', value: 'Scholarly Journal')
+            column(name: 'df_publication_type_uid', value: '46634907-882e-4f91-b1ad-f57db945aff7')
+            column(name: 'publication_type_weight', value: '2')
+            column(name: 'institution', value: 'BIOLA UNIVERSITY')
+            column(name: 'comment', value: 'AACL baseline usage Comment 3')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage_baseline_aacl') {
+            column(name: 'df_usage_baseline_aacl_uid', value: 'a5e00786-c741-4460-b436-da832a285cf8')
+            column(name: 'wr_wrk_inst', value: '123456789')
+            column(name: 'usage_period', value: '2016')
+            column(name: 'usage_source', value: 'Aug 2016 FR')
+            column(name: 'number_of_copies', value: '30')
+            column(name: 'number_of_pages', value: '35')
+            column(name: 'detail_licensee_class_id', value: '143')
+            column(name: 'original_publication_type', value: 'Scholarly Journal')
+            column(name: 'df_publication_type_uid', value: '46634907-882e-4f91-b1ad-f57db945aff7')
+            column(name: 'publication_type_weight', value: '2')
+            column(name: 'institution', value: 'BIOLA UNIVERSITY')
+            column(name: 'comment', value: 'AACL baseline usage Comment 3')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage_archive') {
+            column(name: 'df_usage_archive_uid', value: 'e5ae9237-05a0-4c82-b607-0f91f19b2f24')
+            column(name: 'df_usage_batch_uid', value: 'a87218a0-1966-40aa-846a-434b3fd41282')
+            column(name: 'df_scenario_uid', value: '351e585c-0b08-429d-9e31-bea283ba33de')
+            column(name: 'product_family', value: 'AACL')
+            column(name: 'wr_wrk_inst', value: '243904752')
+            column(name: 'work_title', value: '100 ROAD MOVIES')
+            column(name: 'system_title', value: '100 ROAD MOVIES')
+            column(name: 'rh_account_number', value: '1000002859')
+            column(name: 'payee_account_number', value: '1000002859')
+            column(name: 'status_ind', value: 'PAID')
+            column(name: 'standard_number', value: '1008902112317555XX')
+            column(name: 'gross_amount', value: '500.00')
+            column(name: 'net_amount', value: '420.00')
+            column(name: 'service_fee_amount', value: '80.00')
+            column(name: 'service_fee', value: '0.16000')
+            column(name: 'check_number', value: '578945')
+            column(name: 'check_date', value: '2016-11-03')
+            column(name: 'ccc_event_id', value: '53256')
+            column(name: 'distribution_name', value: 'FDA March 17')
+            column(name: 'distribution_date', value: '2016-03-11')
+            column(name: 'period_end_date', value: '2016-03-11')
+            column(name: 'created_datetime', value: '2016-03-11')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage_aacl') {
+            column(name: 'df_usage_aacl_uid', value: 'e5ae9237-05a0-4c82-b607-0f91f19b2f24')
+            column(name: 'institution', value: 'CORNELL UNIVERSITY')
+            column(name: 'usage_period', value: '2017')
+            column(name: 'usage_source', value: 'Feb 2017 TUR')
+            column(name: 'number_of_pages', value: '1')
+            column(name: 'right_limitation', value: 'PRINT')
+            column(name: 'detail_licensee_class_id', value: '108')
+            column(name: 'df_publication_type_uid', value: '1f6f1925-7aa1-4b1a-b3a8-8903acc3d18e')
+            column(name: 'publication_type_weight', value: '1.71')
+            column(name: 'baseline_uid', value: 'a5e00786-c741-4460-b436-da832a285cf8')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage_archive') {
+            column(name: 'df_usage_archive_uid', value: '8ab89fcc-abf9-432e-b653-e84f2605697f')
+            column(name: 'df_usage_batch_uid', value: 'a87218a0-1966-40aa-846a-434b3fd41282')
+            column(name: 'df_scenario_uid', value: '351e585c-0b08-429d-9e31-bea283ba33de')
+            column(name: 'product_family', value: 'AACL')
+            column(name: 'wr_wrk_inst', value: '243904752')
+            column(name: 'work_title', value: '100 ROAD MOVIES')
+            column(name: 'system_title', value: '100 ROAD MOVIES')
+            column(name: 'rh_account_number', value: '1000002859')
+            column(name: 'payee_account_number', value: '1000002859')
+            column(name: 'status_ind', value: 'PAID')
+            column(name: 'standard_number', value: '1008902112317555XX')
+            column(name: 'gross_amount', value: '500.00')
+            column(name: 'net_amount', value: '420.00')
+            column(name: 'service_fee_amount', value: '80.00')
+            column(name: 'service_fee', value: '0.16000')
+            column(name: 'check_number', value: '578945')
+            column(name: 'check_date', value: '2016-11-03')
+            column(name: 'ccc_event_id', value: '53257')
+            column(name: 'distribution_name', value: 'FDA March 17')
+            column(name: 'distribution_date', value: '2016-03-11')
+            column(name: 'period_end_date', value: '2016-03-11')
+            column(name: 'created_datetime', value: '2016-03-11')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage_aacl') {
+            column(name: 'df_usage_aacl_uid', value: '8ab89fcc-abf9-432e-b653-e84f2605697f')
+            column(name: 'institution', value: 'CORNELL UNIVERSITY')
+            column(name: 'usage_period', value: '2017')
+            column(name: 'usage_source', value: 'Feb 2017 TUR')
+            column(name: 'number_of_pages', value: '1')
+            column(name: 'right_limitation', value: 'PRINT')
+            column(name: 'detail_licensee_class_id', value: '108')
+            column(name: 'df_publication_type_uid', value: '1f6f1925-7aa1-4b1a-b3a8-8903acc3d18e')
+            column(name: 'publication_type_weight', value: '1.71')
+            column(name: 'baseline_uid', value: '9aeb87ed-5000-4bee-abcb-e3dfc4f84235')
+        }
+    }
+}
