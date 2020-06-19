31a6199601216989716b5123b5ac670d8c64548d
StanislauRudak <stanislau_rudak@epam.com>
2020-06-19 04:49:21 -0400

    B-58556 FDA: Tax Notification Report in FDA: implement repository logic for getting RH/payee pairs from scenario

---
+++ dist-foreign-domain/src/main/java/com/copyright/rup/dist/foreign/domain/RightsholderPayeeProductFamilyHolder.java
@@ -0,0 +1,81 @@
+package com.copyright.rup.dist.foreign.domain;
+
+import com.copyright.rup.dist.common.domain.Rightsholder;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+/**
+ * Represents rightsholder, payee and product family combination.
+ * <p>
+ * Copyright (C) 2020 copyright.com
+ * <p>
+ * Date: 6/18/20
+ *
+ * @author Stanislau Rudak
+ */
+public class RightsholderPayeeProductFamilyHolder {
+
+    private Rightsholder rightsholder = new Rightsholder();
+    private Rightsholder payee = new Rightsholder();
+    private String productFamily;
+
+    public Rightsholder getRightsholder() {
+        return rightsholder;
+    }
+
+    public void setRightsholder(Rightsholder rightsholder) {
+        this.rightsholder = rightsholder;
+    }
+
+    public Rightsholder getPayee() {
+        return payee;
+    }
+
+    public void setPayee(Rightsholder payee) {
+        this.payee = payee;
+    }
+
+    public String getProductFamily() {
+        return productFamily;
+    }
+
+    public void setProductFamily(String productFamily) {
+        this.productFamily = productFamily;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (null == o || getClass() != o.getClass()) {
+            return false;
+        }
+        RightsholderPayeeProductFamilyHolder that = (RightsholderPayeeProductFamilyHolder) o;
+        return new EqualsBuilder()
+            .append(rightsholder, that.rightsholder)
+            .append(payee, that.payee)
+            .append(productFamily, that.productFamily)
+            .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+            .append(rightsholder)
+            .append(payee)
+            .append(productFamily)
+            .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+            .append("rightsholder", rightsholder)
+            .append("payee", payee)
+            .append("productFamily", productFamily)
+            .toString();
+    }
+}
--- dist-foreign-repository/src/testInteg/resources/com/copyright/rup/dist/foreign/repository/liquibase/usage-repository-test-data-init.groovy
+++ dist-foreign-repository/src/testInteg/resources/com/copyright/rup/dist/foreign/repository/liquibase/usage-repository-test-data-init.groovy
@@ -1663,4 +1663,248 @@ databaseChangeLog {
             column(name: 'classification', value: 'STM')
         }
     }
+
+    changeSet(id: '2020-06-18-00', author: 'Stanislau Rudak <srudak@copyright.com>') {
+        comment('Insert test data for testFindRightsholderPayeeProductFamilyHoldersByScenarioIds')
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage_batch') {
+            column(name: 'df_usage_batch_uid', value: 'c63948a9-582e-43a5-a8b2-425e6fb59fa1')
+            column(name: 'name', value: 'FAS2 batch 3')
+            column(name: 'rro_account_number', value: '2000017000')
+            column(name: 'payment_date', value: '2019-02-13')
+            column(name: 'product_family', value: 'FAS2')
+            column(name: 'fiscal_year', value: '2020')
+            column(name: 'gross_amount', value: '3000.00')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage_batch') {
+            column(name: 'df_usage_batch_uid', value: '78e8ae36-c400-4167-ab73-7204c4936d1c')
+            column(name: 'name', value: 'FAS batch 4')
+            column(name: 'rro_account_number', value: '1000002859')
+            column(name: 'payment_date', value: '2019-02-13')
+            column(name: 'product_family', value: 'FAS')
+            column(name: 'fiscal_year', value: '2020')
+            column(name: 'gross_amount', value: '3000.00')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_scenario') {
+            column(name: 'df_scenario_uid', value: '05ebb365-fa0d-4329-8a47-0b49968c6b82')
+            column(name: 'name', value: 'FAS2 Scenario')
+            column(name: 'status_ind', value: 'IN_PROGRESS')
+            column(name: 'description', value: 'FAS2 Scenario description')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_scenario') {
+            column(name: 'df_scenario_uid', value: '642b8342-a322-4b3e-afbd-4446cb218841')
+            column(name: 'name', value: 'FAS Scenario')
+            column(name: 'status_ind', value: 'IN_PROGRESS')
+            column(name: 'description', value: 'FAS Scenario description')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_scenario_usage_filter') {
+            column(name: 'df_scenario_usage_filter_uid', value: '44aba2bd-6d05-4892-b5d1-6aa25a0ca7fe')
+            column(name: 'df_scenario_uid', value: '05ebb365-fa0d-4329-8a47-0b49968c6b82')
+            column(name: 'product_family', value: 'FAS2')
+            column(name: 'status_ind', value: 'ELIGIBLE')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_scenario_usage_filter') {
+            column(name: 'df_scenario_usage_filter_uid', value: '18602ce7-ff92-46a3-8946-668274ff98c9')
+            column(name: 'df_scenario_uid', value: '642b8342-a322-4b3e-afbd-4446cb218841')
+            column(name: 'product_family', value: 'FAS2')
+            column(name: 'status_ind', value: 'ELIGIBLE')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage') {
+            column(name: 'df_usage_uid', value: '7b9c14ec-3a38-4e45-90c6-a164f3cb4f83')
+            column(name: 'df_usage_batch_uid', value: 'c63948a9-582e-43a5-a8b2-425e6fb59fa1')
+            column(name: 'df_scenario_uid', value: '05ebb365-fa0d-4329-8a47-0b49968c6b82')
+            column(name: 'wr_wrk_inst', value: '243904752')
+            column(name: 'work_title', value: '100 ROAD MOVIES')
+            column(name: 'rh_account_number', value: '1000002859')
+            column(name: 'payee_account_number', value: '1000002859')
+            column(name: 'status_ind', value: 'LOCKED')
+            column(name: 'product_family', value: 'FAS2')
+            column(name: 'standard_number', value: '1008902112377654XX')
+            column(name: 'number_of_copies', value: '250232')
+            column(name: 'gross_amount', value: '1000')
+            column(name: 'net_amount', value: '680')
+            column(name: 'service_fee_amount', value: '320')
+            column(name: 'service_fee', value: '0.32000')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage_fas') {
+            column(name: 'df_usage_fas_uid', value: '7b9c14ec-3a38-4e45-90c6-a164f3cb4f83')
+            column(name: 'article', value: 'DIN EN 779:2012')
+            column(name: 'publisher', value: 'IEEE')
+            column(name: 'publication_date', value: '2013-09-10')
+            column(name: 'market', value: 'Doc Del')
+            column(name: 'market_period_from', value: '2013')
+            column(name: 'market_period_to', value: '2017')
+            column(name: 'author', value: 'Philippe de Mézières')
+            column(name: 'reported_value', value: '9900')
+            column(name: 'is_rh_participating_flag', value: true)
+            column(name: 'is_payee_participating_flag', value: true)
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage') {
+            column(name: 'df_usage_uid', value: '2c513e62-5bba-4a9d-be79-4604a7b3c18c')
+            column(name: 'df_usage_batch_uid', value: 'c63948a9-582e-43a5-a8b2-425e6fb59fa1')
+            column(name: 'df_scenario_uid', value: '05ebb365-fa0d-4329-8a47-0b49968c6b82')
+            column(name: 'wr_wrk_inst', value: '243904752')
+            column(name: 'work_title', value: '100 ROAD MOVIES')
+            column(name: 'rh_account_number', value: '1000002859')
+            column(name: 'payee_account_number', value: '1000002859')
+            column(name: 'status_ind', value: 'LOCKED')
+            column(name: 'product_family', value: 'FAS2')
+            column(name: 'standard_number', value: '1008902112317622XX')
+            column(name: 'number_of_copies', value: '100')
+            column(name: 'gross_amount', value: '1000')
+            column(name: 'net_amount', value: '680')
+            column(name: 'service_fee_amount', value: '320')
+            column(name: 'service_fee', value: '0.32000')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage_fas') {
+            column(name: 'df_usage_fas_uid', value: '2c513e62-5bba-4a9d-be79-4604a7b3c18c')
+            column(name: 'article', value: 'DIN EN 779:2012')
+            column(name: 'publisher', value: 'IEEE')
+            column(name: 'publication_date', value: '2013-09-10')
+            column(name: 'market', value: 'Doc Del')
+            column(name: 'market_period_from', value: '2013')
+            column(name: 'market_period_to', value: '2017')
+            column(name: 'author', value: 'Philippe de Mézières')
+            column(name: 'reported_value', value: '9900')
+            column(name: 'is_rh_participating_flag', value: true)
+            column(name: 'is_payee_participating_flag', value: true)
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage') {
+            column(name: 'df_usage_uid', value: '76d595db-d249-4f1d-a9bc-291f00729fd5')
+            column(name: 'df_usage_batch_uid', value: 'c63948a9-582e-43a5-a8b2-425e6fb59fa1')
+            column(name: 'df_scenario_uid', value: '05ebb365-fa0d-4329-8a47-0b49968c6b82')
+            column(name: 'wr_wrk_inst', value: '243904752')
+            column(name: 'work_title', value: '100 ROAD MOVIES')
+            column(name: 'rh_account_number', value: '1000002859')
+            column(name: 'payee_account_number', value: '1000005413')
+            column(name: 'status_ind', value: 'LOCKED')
+            column(name: 'product_family', value: 'FAS2')
+            column(name: 'standard_number', value: '1008902112317622XX')
+            column(name: 'number_of_copies', value: '100')
+            column(name: 'gross_amount', value: '1000')
+            column(name: 'net_amount', value: '680')
+            column(name: 'service_fee_amount', value: '320')
+            column(name: 'service_fee', value: '0.32000')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage_fas') {
+            column(name: 'df_usage_fas_uid', value: '76d595db-d249-4f1d-a9bc-291f00729fd5')
+            column(name: 'article', value: 'DIN EN 779:2012')
+            column(name: 'publisher', value: 'IEEE')
+            column(name: 'publication_date', value: '2013-09-10')
+            column(name: 'market', value: 'Doc Del')
+            column(name: 'market_period_from', value: '2013')
+            column(name: 'market_period_to', value: '2017')
+            column(name: 'author', value: 'Philippe de Mézières')
+            column(name: 'reported_value', value: '9900')
+            column(name: 'is_rh_participating_flag', value: true)
+            column(name: 'is_payee_participating_flag', value: true)
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage') {
+            column(name: 'df_usage_uid', value: 'cd9307a8-1eeb-4709-a4e2-b6be84771104')
+            column(name: 'df_usage_batch_uid', value: '78e8ae36-c400-4167-ab73-7204c4936d1c')
+            column(name: 'df_scenario_uid', value: '642b8342-a322-4b3e-afbd-4446cb218841')
+            column(name: 'wr_wrk_inst', value: '243904752')
+            column(name: 'work_title', value: '100 ROAD MOVIES')
+            column(name: 'rh_account_number', value: '1000002859')
+            column(name: 'payee_account_number', value: '1000002859')
+            column(name: 'status_ind', value: 'LOCKED')
+            column(name: 'product_family', value: 'FAS')
+            column(name: 'standard_number', value: '1008902112377654XX')
+            column(name: 'number_of_copies', value: '250232')
+            column(name: 'gross_amount', value: '1000')
+            column(name: 'net_amount', value: '680')
+            column(name: 'service_fee_amount', value: '320')
+            column(name: 'service_fee', value: '0.32000')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage_fas') {
+            column(name: 'df_usage_fas_uid', value: 'cd9307a8-1eeb-4709-a4e2-b6be84771104')
+            column(name: 'article', value: 'DIN EN 779:2012')
+            column(name: 'publisher', value: 'IEEE')
+            column(name: 'publication_date', value: '2013-09-10')
+            column(name: 'market', value: 'Doc Del')
+            column(name: 'market_period_from', value: '2013')
+            column(name: 'market_period_to', value: '2017')
+            column(name: 'author', value: 'Philippe de Mézières')
+            column(name: 'reported_value', value: '9900')
+            column(name: 'is_rh_participating_flag', value: true)
+            column(name: 'is_payee_participating_flag', value: true)
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage') {
+            column(name: 'df_usage_uid', value: '37b74bce-8a03-4110-81de-d79958376590')
+            column(name: 'df_usage_batch_uid', value: '78e8ae36-c400-4167-ab73-7204c4936d1c')
+            column(name: 'df_scenario_uid', value: '642b8342-a322-4b3e-afbd-4446cb218841')
+            column(name: 'wr_wrk_inst', value: '243904752')
+            column(name: 'work_title', value: '100 ROAD MOVIES')
+            column(name: 'rh_account_number', value: '1000002859')
+            column(name: 'payee_account_number', value: '1000005413')
+            column(name: 'status_ind', value: 'LOCKED')
+            column(name: 'product_family', value: 'FAS')
+            column(name: 'standard_number', value: '1008902112317622XX')
+            column(name: 'number_of_copies', value: '100')
+            column(name: 'gross_amount', value: '1000')
+            column(name: 'net_amount', value: '680')
+            column(name: 'service_fee_amount', value: '320')
+            column(name: 'service_fee', value: '0.32000')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage_fas') {
+            column(name: 'df_usage_fas_uid', value: '37b74bce-8a03-4110-81de-d79958376590')
+            column(name: 'article', value: 'DIN EN 779:2012')
+            column(name: 'publisher', value: 'IEEE')
+            column(name: 'publication_date', value: '2013-09-10')
+            column(name: 'market', value: 'Doc Del')
+            column(name: 'market_period_from', value: '2013')
+            column(name: 'market_period_to', value: '2017')
+            column(name: 'author', value: 'Philippe de Mézières')
+            column(name: 'reported_value', value: '9900')
+            column(name: 'is_rh_participating_flag', value: true)
+            column(name: 'is_payee_participating_flag', value: true)
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage') {
+            column(name: 'df_usage_uid', value: '3f06d5e2-81d5-4e12-a784-a7485066d5f3')
+            column(name: 'df_usage_batch_uid', value: '78e8ae36-c400-4167-ab73-7204c4936d1c')
+            column(name: 'df_scenario_uid', value: '642b8342-a322-4b3e-afbd-4446cb218841')
+            column(name: 'wr_wrk_inst', value: '243904752')
+            column(name: 'work_title', value: '100 ROAD MOVIES')
+            column(name: 'rh_account_number', value: '1000002797')
+            column(name: 'payee_account_number', value: '1000002859')
+            column(name: 'status_ind', value: 'LOCKED')
+            column(name: 'product_family', value: 'FAS')
+            column(name: 'standard_number', value: '1008902112317622XX')
+            column(name: 'number_of_copies', value: '100')
+            column(name: 'gross_amount', value: '1000')
+            column(name: 'net_amount', value: '680')
+            column(name: 'service_fee_amount', value: '320')
+            column(name: 'service_fee', value: '0.32000')
+        }
+
+        insert(schemaName: dbAppsSchema, tableName: 'df_usage_fas') {
+            column(name: 'df_usage_fas_uid', value: '3f06d5e2-81d5-4e12-a784-a7485066d5f3')
+            column(name: 'article', value: 'DIN EN 779:2012')
+            column(name: 'publisher', value: 'IEEE')
+            column(name: 'publication_date', value: '2013-09-10')
+            column(name: 'market', value: 'Doc Del')
+            column(name: 'market_period_from', value: '2013')
+            column(name: 'market_period_to', value: '2017')
+            column(name: 'author', value: 'Philippe de Mézières')
+            column(name: 'reported_value', value: '9900')
+            column(name: 'is_rh_participating_flag', value: true)
+            column(name: 'is_payee_participating_flag', value: true)
+        }
+    }
 }
