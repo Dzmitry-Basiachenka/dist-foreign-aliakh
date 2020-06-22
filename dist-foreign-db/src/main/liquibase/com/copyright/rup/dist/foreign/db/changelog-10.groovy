b254e215bc167c8da6a836e53bcf7f75d60d7326
antonAzarenka <antonAzarenka@github.com>
2020-06-22 09:26:35 +0300

    B-58560 FDA: View/Export NTS withdrawn details on FAS/FAS2 usage tab: update historical data, change product family from NTS to FAS/FAS2 for details with NTS_WITHDRAWN & TO_BE_DISTRIBUTED statuses for archived usages

--- dist-foreign-db/src/main/liquibase/com/copyright/rup/dist/foreign/db/changelog-10.groovy
+++ dist-foreign-db/src/main/liquibase/com/copyright/rup/dist/foreign/db/changelog-10.groovy
@@ -17,4 +17,24 @@ databaseChangeLog {
                 """)
         }
     }
+
+    changeSet(id: '2020-06-19-00', author: 'Anton Azarenka <aazarenka@copyright.com>') {
+        comment("B-58560 FDA: View/Export NTS withdrawn details on FAS/FAS2 usage tab: change product family " +
+                "from NTS to FAS/FAS2 for details with NTS_WITHDRAWN and TO_BE_DISTRIBUTED statuses for archived details")
+
+        sql("""update ${dbAppsSchema}.df_usage_archive ua set product_family = b.product_family
+                from ${dbAppsSchema}.df_usage_batch b, ${dbAppsSchema}.df_usage_fas uf
+                where ua.df_usage_batch_uid = b.df_usage_batch_uid
+                    and ua.df_usage_archive_uid = uf.df_usage_fas_uid
+                    and uf.df_fund_pool_uid is not null
+                """)
+
+        rollback {
+            sql("""update ${dbAppsSchema}.df_usage_archive ua set product_family = 'NTS'
+                from ${dbAppsSchema}.df_usage_fas uf
+                where uf.df_usage_fas_uid = ua.df_usage_archive_uid
+                    and uf.df_fund_pool_uid is not null
+                """)
+        }
+    }
 }
