d62925115fc2e85164d72bece4265996d389f1dd
Aliaksandr_Liakh <Aliaksandr_Liakh1@epam.com>
2020-07-29 08:10:09 -0400

    B-57883 FDA: SAL UI usage view: implement repository to read SAL usages

---
+++ dist-foreign-repository/src/main/java/com/copyright/rup/dist/foreign/repository/api/ISalUsageRepository.java
@@ -0,0 +1,38 @@
+package com.copyright.rup.dist.foreign.repository.api;
+
+import com.copyright.rup.dist.common.repository.api.Pageable;
+import com.copyright.rup.dist.common.repository.api.Sort;
+import com.copyright.rup.dist.foreign.domain.UsageDto;
+import com.copyright.rup.dist.foreign.domain.filter.UsageFilter;
+
+import java.util.List;
+
+/**
+ * Interface for SAL usages repository.
+ * <p>
+ * Copyright (C) 2020 copyright.com
+ * <p>
+ * Date: 07/29/2020
+ *
+ * @author Aliaksandr Liakh
+ */
+public interface ISalUsageRepository {
+
+    /**
+     * Finds usages count by usage filter.
+     *
+     * @param filter instance of {@link UsageFilter}
+     * @return the count of usages
+     */
+    int findCountByFilter(UsageFilter filter);
+
+    /**
+     * Finds list of {@link UsageDto}s by usage filter.
+     *
+     * @param filter   instance of {@link UsageFilter}
+     * @param pageable instance of {@link Pageable}
+     * @param sort     instance of {@link Sort}
+     * @return the list of {@link UsageDto}
+     */
+    List<UsageDto> findDtosByFilter(UsageFilter filter, Pageable pageable, Sort sort);
+}
