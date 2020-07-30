b70e704471bc673649fa8c5121903dfdae5e11cc
Uladzislau_Shalamitski <Uladzislau_Shalamitski@epam.com>
2020-07-30 04:40:51 -0400

    B-57883 FDA: SAL UI usage view: implement filters on Usages tab

--- dist-foreign-ui/src/main/java/com/copyright/rup/dist/foreign/ui/usage/impl/sal/SalUsageFilterWidget.java
+++ dist-foreign-ui/src/main/java/com/copyright/rup/dist/foreign/ui/usage/impl/sal/SalUsageFilterWidget.java
@@ -1,9 +1,23 @@
 package com.copyright.rup.dist.foreign.ui.usage.impl.sal;
 
+import com.copyright.rup.dist.foreign.domain.SalDetailTypeEnum;
+import com.copyright.rup.dist.foreign.domain.UsageBatch;
+import com.copyright.rup.dist.foreign.domain.UsageStatusEnum;
+import com.copyright.rup.dist.foreign.ui.common.UsageBatchFilterWidget;
+import com.copyright.rup.dist.foreign.ui.main.ForeignUi;
 import com.copyright.rup.dist.foreign.ui.usage.api.aacl.IAaclUsageFilterWidget;
+import com.copyright.rup.dist.foreign.ui.usage.api.aacl.ISalUsageFilterController;
 import com.copyright.rup.dist.foreign.ui.usage.impl.CommonUsageFilterWidget;
+import com.copyright.rup.vaadin.ui.component.filter.FilterWindow;
+import com.copyright.rup.vaadin.util.VaadinUtils;
 
+import com.google.common.collect.ImmutableSet;
+import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.VerticalLayout;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Widget for filtering SAL usages.
@@ -16,21 +30,75 @@ import com.vaadin.ui.VerticalLayout;
  */
 public class SalUsageFilterWidget extends CommonUsageFilterWidget implements IAaclUsageFilterWidget {
 
+    private static final Set<UsageStatusEnum> SAL_STATUSES = ImmutableSet.of(UsageStatusEnum.NEW);
+
+    private final ISalUsageFilterController controller;
+
+    private UsageBatchFilterWidget usageBatchFilterWidget;
+    private ComboBox<UsageStatusEnum> statusComboBox;
+    private ComboBox<SalDetailTypeEnum> detailTypeComboBox;
+
+    /**
+     * Constructor.
+     *
+     * @param controller instance of {@link ISalUsageFilterController}
+     */
+    public SalUsageFilterWidget(ISalUsageFilterController controller) {
+        this.controller = controller;
+    }
+
     @Override
     protected void refreshFilterValues() {
-        //TODO: implement refresh of filter values
+        statusComboBox.setItems(SAL_STATUSES);
+        detailTypeComboBox.setItems(SalDetailTypeEnum.values());
     }
 
     @Override
     protected void clearFilterValues() {
-        //TODO: implement clear of filter values
+        usageBatchFilterWidget.reset();
+        statusComboBox.clear();
+        detailTypeComboBox.clear();
     }
 
     @Override
     protected VerticalLayout initFiltersLayout() {
-        //TODO: implement filters
-        VerticalLayout verticalLayout = new VerticalLayout(buildFiltersHeaderLabel());
+        initStatusFilter();
+        initDetailTypeFilter();
+        VerticalLayout verticalLayout =
+            new VerticalLayout(buildFiltersHeaderLabel(), buildUsageBatchFilter(), statusComboBox, detailTypeComboBox);
         verticalLayout.setMargin(false);
         return verticalLayout;
+    }
+
+    private HorizontalLayout buildUsageBatchFilter() {
+        usageBatchFilterWidget = new UsageBatchFilterWidget(controller::getUsageBatches);
+        usageBatchFilterWidget.addFilterSaveListener((FilterWindow.IFilterSaveListener<UsageBatch>) saveEvent -> {
+            getFilter().setUsageBatchesIds(saveEvent.getSelectedItemsIds().stream()
+                .map(UsageBatch::getId)
+                .collect(Collectors.toSet()));
+            filterChanged();
+        });
+        VaadinUtils.addComponentStyle(usageBatchFilterWidget, "batches-filter");
+        return usageBatchFilterWidget;
+    }
+
+    private void initStatusFilter() {
+        statusComboBox = new ComboBox<>(ForeignUi.getMessage("label.status"));
+        statusComboBox.addValueChangeListener(event -> {
+            getFilter().setUsageStatus(statusComboBox.getValue());
+            filterChanged();
+        });
+        VaadinUtils.setMaxComponentsWidth(statusComboBox);
+        VaadinUtils.addComponentStyle(statusComboBox, "status-filter");
+    }
+
+    private void initDetailTypeFilter() {
+        detailTypeComboBox = new ComboBox<>(ForeignUi.getMessage("label.detail_type"));
+        detailTypeComboBox.addValueChangeListener(event -> {
+            getFilter().setSalDetailType(detailTypeComboBox.getValue());
+            filterChanged();
+        });
+        VaadinUtils.setMaxComponentsWidth(detailTypeComboBox);
+        VaadinUtils.addComponentStyle(detailTypeComboBox, "detail-type-filter");
     }
 }
---
+++ dist-foreign-ui/src/test/java/com/copyright/rup/dist/foreign/ui/usage/impl/sal/SalUsageFilterWidgetTest.java
@@ -0,0 +1,271 @@
+package com.copyright.rup.dist.foreign.ui.usage.impl.sal;
+
+import static org.easymock.EasyMock.expect;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.powermock.api.easymock.PowerMock.createMock;
+import static org.powermock.api.easymock.PowerMock.expectLastCall;
+import static org.powermock.api.easymock.PowerMock.mockStatic;
+import static org.powermock.api.easymock.PowerMock.replay;
+import static org.powermock.api.easymock.PowerMock.verify;
+
+import com.copyright.rup.dist.foreign.domain.SalDetailTypeEnum;
+import com.copyright.rup.dist.foreign.domain.UsageStatusEnum;
+import com.copyright.rup.dist.foreign.ui.usage.api.aacl.ISalUsageFilterController;
+import com.copyright.rup.vaadin.ui.component.window.Windows;
+import com.copyright.rup.vaadin.ui.themes.Cornerstone;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+import com.vaadin.data.provider.ListDataProvider;
+import com.vaadin.server.Sizeable.Unit;
+import com.vaadin.shared.ui.MarginInfo;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Verifies {@link SalUsageFilterWidget}.
+ * <p>
+ * Copyright (C) 2020 copyright.com
+ * <p>
+ * Date: 07/28/2020
+ *
+ * @author Uladzislau Shalamitski
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Windows.class)
+public class SalUsageFilterWidgetTest {
+
+    private static final Long ACCOUNT_NUMBER = 12345678L;
+    private static final String SAL_PRODUCT_FAMILY = "SAL";
+
+    private static final Set<UsageStatusEnum> SAL_STATUSES = ImmutableSet.of(UsageStatusEnum.NEW);
+
+    private ISalUsageFilterController usagesFilterController;
+    private SalUsageFilterWidget widget;
+
+    @Before
+    public void setUp() {
+        usagesFilterController = createMock(ISalUsageFilterController.class);
+        widget = new SalUsageFilterWidget(usagesFilterController);
+        widget.setController(usagesFilterController);
+    }
+
+    @Test
+    public void testInit() {
+        expect(usagesFilterController.getSelectedProductFamily()).andReturn(SAL_PRODUCT_FAMILY).once();
+        replay(usagesFilterController);
+        assertSame(widget, widget.init());
+        assertEquals(2, widget.getComponentCount());
+        assertEquals(new MarginInfo(true), widget.getMargin());
+        verifyFiltersLayout(widget.getComponent(0));
+        verifyButtonsLayout(widget.getComponent(1));
+        verify(usagesFilterController);
+    }
+
+    @Test
+    public void testApplyFilter() {
+        expect(usagesFilterController.getSelectedProductFamily()).andReturn(SAL_PRODUCT_FAMILY).times(2);
+        replay(usagesFilterController);
+        widget.init();
+        widget.clearFilter();
+        verify(usagesFilterController);
+        Button applyButton = getApplyButton();
+        assertFalse(applyButton.isEnabled());
+        assertTrue(widget.getAppliedFilter().getRhAccountNumbers().isEmpty());
+        assertTrue(widget.getFilter().getRhAccountNumbers().isEmpty());
+        Set<Long> accountNumbers = Sets.newHashSet(ACCOUNT_NUMBER);
+        widget.getFilter().setRhAccountNumbers(accountNumbers);
+        assertNotEquals(widget.getFilter(), widget.getAppliedFilter());
+        applyButton.setEnabled(true);
+        assertTrue(widget.getAppliedFilter().getRhAccountNumbers().isEmpty());
+        assertFalse(widget.getFilter().getRhAccountNumbers().isEmpty());
+        assertTrue(applyButton.isEnabled());
+        applyButton.click();
+        assertFalse(applyButton.isEnabled());
+        assertFalse(widget.getAppliedFilter().getRhAccountNumbers().isEmpty());
+    }
+
+    @Test
+    public void testFilterChangedEmptyFilter() {
+        expect(usagesFilterController.getSelectedProductFamily()).andReturn(SAL_PRODUCT_FAMILY).once();
+        replay(usagesFilterController);
+        widget.init();
+        Button applyButton = getApplyButton();
+        assertFalse(applyButton.isEnabled());
+        widget.applyFilter();
+        verify(usagesFilterController);
+        assertFalse(applyButton.isEnabled());
+    }
+
+    @Test
+    public void testClearFilter() {
+        expect(usagesFilterController.getSelectedProductFamily()).andReturn(SAL_PRODUCT_FAMILY).times(2);
+        replay(usagesFilterController);
+        widget.init();
+        Button applyButton = getApplyButton();
+        assertTrue(widget.getFilter().isEmpty());
+        assertTrue(widget.getAppliedFilter().isEmpty());
+        assertEquals(SAL_PRODUCT_FAMILY, widget.getFilter().getProductFamily());
+        assertNull(widget.getAppliedFilter().getProductFamily());
+        widget.getFilter().setSalDetailType(SalDetailTypeEnum.IB);
+        widget.getFilter().setProductFamily(SAL_PRODUCT_FAMILY);
+        applyButton.setEnabled(true);
+        applyButton.click();
+        assertFalse(widget.getFilter().isEmpty());
+        assertFalse(widget.getAppliedFilter().isEmpty());
+        assertEquals(SAL_PRODUCT_FAMILY, widget.getFilter().getProductFamily());
+        assertEquals(SAL_PRODUCT_FAMILY, widget.getAppliedFilter().getProductFamily());
+        widget.clearFilter();
+        assertTrue(widget.getFilter().isEmpty());
+        assertTrue(widget.getAppliedFilter().isEmpty());
+        assertEquals(SAL_PRODUCT_FAMILY, widget.getFilter().getProductFamily());
+        assertEquals(SAL_PRODUCT_FAMILY, widget.getAppliedFilter().getProductFamily());
+        assertFalse(applyButton.isEnabled());
+        ComboBox detailTypeComboBox = Whitebox.getInternalState(widget, "detailTypeComboBox");
+        assertNull(detailTypeComboBox.getValue());
+        verify(usagesFilterController);
+    }
+
+    @Test
+    public void verifyApplyButtonClickListener() {
+        expect(usagesFilterController.getSelectedProductFamily()).andReturn(SAL_PRODUCT_FAMILY).once();
+        mockStatic(Windows.class);
+        ClickEvent clickEvent = createMock(ClickEvent.class);
+        Windows.showNotificationWindow("Apply filter clicked");
+        expectLastCall().once();
+        replay(clickEvent, Windows.class, usagesFilterController);
+        widget.init();
+        ClickListener clickListener = (ClickListener) getApplyButton().getListeners(ClickEvent.class).iterator().next();
+        clickListener.buttonClick(clickEvent);
+        verify(clickEvent, usagesFilterController);
+    }
+
+    @Test
+    public void verifyButtonClickListener() {
+        expect(usagesFilterController.getSelectedProductFamily()).andReturn(SAL_PRODUCT_FAMILY).times(2);
+        ClickEvent clickEvent = createMock(ClickEvent.class);
+        replay(clickEvent, usagesFilterController);
+        widget.init();
+        Button applyButton = getApplyButton();
+        widget.getFilter().setSalDetailType(SalDetailTypeEnum.IB);
+        applyButton.setEnabled(true);
+        assertFalse(widget.getFilter().isEmpty());
+        Button clearButton = (Button) ((HorizontalLayout) widget.getComponent(1)).getComponent(1);
+        ClickListener clickListener = (ClickListener) clearButton.getListeners(ClickEvent.class).iterator().next();
+        clickListener.buttonClick(clickEvent);
+        assertFalse(applyButton.isEnabled());
+        assertTrue(widget.getFilter().isEmpty());
+        verify(clickEvent, usagesFilterController);
+    }
+
+    private void verifyFiltersLayout(Component layout) {
+        assertTrue(layout instanceof VerticalLayout);
+        VerticalLayout verticalLayout = (VerticalLayout) layout;
+        assertEquals(4, verticalLayout.getComponentCount());
+        verifyFiltersLabel(verticalLayout.getComponent(0));
+        verifyItemsFilterLayout(verticalLayout.getComponent(1), "Batches");
+        verifyUsageStatusCombobox(verticalLayout.getComponent(2));
+        verifyDetailTypeCombobox(verticalLayout.getComponent(3));
+    }
+
+    private void verifyFiltersLabel(Component component) {
+        assertTrue(component instanceof Label);
+        Label label = (Label) component;
+        assertEquals("Filters", label.getValue());
+        assertEquals(Cornerstone.LABEL_H2, label.getStyleName());
+    }
+
+    private void verifyItemsFilterLayout(Component component, String buttonCaption) {
+        assertTrue(component instanceof HorizontalLayout);
+        HorizontalLayout layout = (HorizontalLayout) component;
+        assertTrue(layout.isSpacing());
+        Iterator<Component> iterator = layout.iterator();
+        assertEquals("(0)", ((Label) iterator.next()).getValue());
+        Button button = (Button) iterator.next();
+        assertEquals(buttonCaption, button.getCaption());
+        assertEquals(2, button.getListeners(ClickEvent.class).size());
+        assertTrue(button.isDisableOnClick());
+        assertTrue(StringUtils.contains(button.getStyleName(), Cornerstone.BUTTON_LINK));
+        assertFalse(iterator.hasNext());
+    }
+
+    private void verifyUsageStatusCombobox(Component component) {
+        assertTrue(component instanceof ComboBox);
+        ComboBox comboBox = (ComboBox) component;
+        assertEquals("Status", comboBox.getCaption());
+        assertEquals(100, comboBox.getWidth(), 0);
+        assertEquals(Unit.PERCENTAGE, comboBox.getWidthUnits());
+        ListDataProvider<UsageStatusEnum> listDataProvider =
+            (ListDataProvider<UsageStatusEnum>) comboBox.getDataProvider();
+        Collection<?> actualStatuses = listDataProvider.getItems();
+        assertEquals(1, actualStatuses.size());
+        assertEquals(SAL_STATUSES, actualStatuses);
+    }
+
+    private void verifyDetailTypeCombobox(Component component) {
+        assertTrue(component instanceof ComboBox);
+        ComboBox comboBox = (ComboBox) component;
+        assertEquals("Detail Type", comboBox.getCaption());
+        assertEquals(100, comboBox.getWidth(), 0);
+        assertEquals(Unit.PERCENTAGE, comboBox.getWidthUnits());
+        ListDataProvider<SalDetailTypeEnum> listDataProvider =
+            (ListDataProvider<SalDetailTypeEnum>) comboBox.getDataProvider();
+        Collection<?> actualTypes = listDataProvider.getItems();
+        assertEquals(2, actualTypes.size());
+        assertEquals(Arrays.asList(SalDetailTypeEnum.values()), actualTypes);
+    }
+
+    private void verifyButtonsLayout(Component component) {
+        assertTrue(component instanceof HorizontalLayout);
+        HorizontalLayout layout = (HorizontalLayout) component;
+        assertEquals(2, layout.getComponentCount());
+        assertTrue(layout.isSpacing());
+        verifyButton(layout.getComponent(0), "Apply");
+        verifyButton(layout.getComponent(1), "Clear");
+    }
+
+    private void verifyButton(Component component, String caption) {
+        assertTrue(component instanceof Button);
+        Button button = (Button) component;
+        assertEquals(caption, button.getCaption());
+        assertEquals(100, button.getWidth(), 0);
+        assertEquals(Unit.PERCENTAGE, button.getWidthUnits());
+        verifyButtonClickListener(button);
+    }
+
+    private void verifyButtonClickListener(Button button) {
+        Collection<?> listeners = button.getListeners(ClickEvent.class);
+        assertTrue(CollectionUtils.isNotEmpty(listeners));
+        assertEquals(1, listeners.size());
+        assertNotNull(listeners.iterator().next());
+    }
+
+    private Button getApplyButton() {
+        return Whitebox.getInternalState(widget, "applyButton");
+    }
+}
