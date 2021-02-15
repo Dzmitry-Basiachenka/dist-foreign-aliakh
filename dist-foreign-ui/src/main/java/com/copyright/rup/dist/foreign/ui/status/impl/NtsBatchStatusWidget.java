3cac6ab4fac4d3c1589bab21f4dc9b28d358e75c
DaryaBaraukova <Darya_Baraukova@epam.com>
2021-02-15 03:22:49 -0500

    B-65718 FDA: Batch Processing View: implement Batch Status widget for NTS

--- dist-foreign-ui/src/main/java/com/copyright/rup/dist/foreign/ui/status/impl/NtsBatchStatusWidget.java
+++ dist-foreign-ui/src/main/java/com/copyright/rup/dist/foreign/ui/status/impl/NtsBatchStatusWidget.java
@@ -1,5 +1,6 @@
 package com.copyright.rup.dist.foreign.ui.status.impl;
 
+import com.copyright.rup.dist.foreign.domain.UsageBatchStatus;
 import com.copyright.rup.dist.foreign.ui.status.api.INtsBatchStatusWidget;
 
 /**
@@ -12,7 +13,16 @@ import com.copyright.rup.dist.foreign.ui.status.api.INtsBatchStatusWidget;
  * @author Ihar Suvorau
  */
 public class NtsBatchStatusWidget extends CommonBatchStatusWidget implements INtsBatchStatusWidget {
+
     @Override
     protected void addColumns() {
+        addColumn(UsageBatchStatus::getBatchName, "table.column.batch_name", "batchName", true);
+        addColumn(UsageBatchStatus::getTotalCount, "table.column.total_count", "totalCount", true, 130);
+        addColumn(UsageBatchStatus::getWorkFoundCount, "table.column.work_found", "workFoundCount", true, 130);
+        addColumn(UsageBatchStatus::getRhFoundCount, "table.column.rh_found", "rhFoundCount", true, 130);
+        addColumn(UsageBatchStatus::getUnclassifiedCount, "table.column.unclassified", "unclassifiedCount", true, 130);
+        addColumn(UsageBatchStatus::getEligibleCount, "table.column.eligible", "eligibleCount", true, 130);
+        addColumn(UsageBatchStatus::getExcludedCount, "table.column.excluded", "excludedCount", true, 130);
+        addColumn(UsageBatchStatus::getStatus, "table.column.status", "status", true);
     }
 }
---
+++ dist-foreign-ui/src/test/java/com/copyright/rup/dist/foreign/ui/status/impl/NtsBatchStatusWidgetTest.java
@@ -0,0 +1,67 @@
+package com.copyright.rup.dist.foreign.ui.status.impl;
+
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.copyright.rup.dist.foreign.ui.status.api.ICommonBatchStatusController;
+import com.vaadin.server.Sizeable;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Grid;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Verifies {@link NtsBatchStatusWidget}.
+ * <p>
+ * Copyright (C) 2021 copyright.com
+ * <p>
+ * Date: 02/15/21
+ *
+ * @author Darya Baraukova
+ */
+public class NtsBatchStatusWidgetTest {
+
+    private NtsBatchStatusWidget batchStatusWidget;
+    private ICommonBatchStatusController controller;
+
+    @Before
+    public void setUp() {
+        controller = createMock(ICommonBatchStatusController.class);
+        batchStatusWidget = new NtsBatchStatusWidget();
+        batchStatusWidget.setController(controller);
+    }
+
+    @Test
+    public void testComponentStructure() {
+        expect(controller.getBatchStatuses()).andReturn(Collections.emptyList()).once();
+        replay(controller);
+        batchStatusWidget.init();
+        assertEquals(1, batchStatusWidget.getComponentCount());
+        Component component = batchStatusWidget.getComponent(0);
+        assertTrue(component instanceof Grid);
+        verifyGrid((Grid) component);
+        verify(controller);
+    }
+
+    private void verifyGrid(Grid grid) {
+        assertEquals(100, grid.getWidth(), 0);
+        assertEquals(100, grid.getHeight(), 0);
+        assertEquals(Sizeable.Unit.PERCENTAGE, grid.getHeightUnits());
+        assertEquals(Sizeable.Unit.PERCENTAGE, grid.getWidthUnits());
+        assertEquals("batch-status-grid", grid.getId());
+        List<Grid.Column> columns = grid.getColumns();
+        assertEquals(Arrays.asList("Usage Batch Name", "Total Count", "Work Found", "RH Found", "Unclassified",
+            "Eligible", "Excluded", "Status"),
+            columns.stream().map(Grid.Column::getCaption).collect(Collectors.toList()));
+        columns.forEach(column -> assertTrue(column.isSortable()));
+    }
+}
