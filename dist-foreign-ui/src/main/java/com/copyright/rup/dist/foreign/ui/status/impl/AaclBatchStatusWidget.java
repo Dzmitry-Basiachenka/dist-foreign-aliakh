5a24ba6ec5e71a3662254f51bd3c71647b515e14
DaryaBaraukova <Darya_Baraukova@epam.com>
2021-02-15 04:23:33 -0500

    B-65718 FDA: Batch Processing View: implement Batch Status widget for AACL and SAL

--- dist-foreign-ui/src/main/java/com/copyright/rup/dist/foreign/ui/status/impl/AaclBatchStatusWidget.java
+++ dist-foreign-ui/src/main/java/com/copyright/rup/dist/foreign/ui/status/impl/AaclBatchStatusWidget.java
@@ -1,5 +1,6 @@
 package com.copyright.rup.dist.foreign.ui.status.impl;
 
+import com.copyright.rup.dist.foreign.domain.UsageBatchStatus;
 import com.copyright.rup.dist.foreign.ui.status.api.IAaclBatchStatusWidget;
 
 /**
@@ -15,5 +16,14 @@ public class AaclBatchStatusWidget extends CommonBatchStatusWidget implements IA
 
     @Override
     protected void addColumns() {
+        addColumn(UsageBatchStatus::getBatchName, "table.column.batch_name", "batchName", true);
+        addColumn(UsageBatchStatus::getTotalCount, "table.column.total_count", "totalCount", true, 130);
+        addColumn(UsageBatchStatus::getNewCount, "table.column.new", "newCount", true, 130);
+        addColumn(UsageBatchStatus::getWorkNotFoundCount, "table.column.work_not_found", "workNotFoundCount", true, 130);
+        addColumn(UsageBatchStatus::getWorkFoundCount, "table.column.work_found", "workFoundCount", true, 130);
+        addColumn(UsageBatchStatus::getRhFoundCount, "table.column.rh_found", "rhFoundCount", true, 130);
+        addColumn(UsageBatchStatus::getEligibleCount, "table.column.eligible", "eligibleCount", true, 130);
+        addColumn(UsageBatchStatus::getExcludedCount, "table.column.excluded", "excludedCount", true, 130);
+        addColumn(UsageBatchStatus::getStatus, "table.column.status", "status", true);
     }
 }
