68d42d504a0a98bcf99870a29384c13820f32070
Aliaksandr_Liakh <Aliaksandr_Liakh1@epam.com>
2021-10-19 03:16:11 -0400

    B-67673 FDA: Value Edits: add missed import

--- dist-foreign-ui/src/main/java/com/copyright/rup/dist/foreign/ui/usage/impl/acl/udm/usage/UdmUsageWidget.java
+++ dist-foreign-ui/src/main/java/com/copyright/rup/dist/foreign/ui/usage/impl/acl/udm/usage/UdmUsageWidget.java
@@ -40,6 +40,7 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
