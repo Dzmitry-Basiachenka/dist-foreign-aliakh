bf200c156302a22fafa9bf52b1769b42eabb5f36
Dzmitry Basiachenka <dzmitry_basiachenka@epam.com>
2021-11-04 17:44:49 +0300

    B-69880: FDA: Swagger 3 for FDA & LM: migrate to Swagger 3

--- dist-foreign-ui/src/main/java/com/copyright/rup/dist/foreign/ui/rest/gen/api/StatisticUsageApiController.java
+++ dist-foreign-ui/src/main/java/com/copyright/rup/dist/foreign/ui/rest/gen/api/StatisticUsageApiController.java
@@ -1,20 +1,22 @@
 package com.copyright.rup.dist.foreign.ui.rest.gen.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Optional;
 
 @Controller
+@RequestMapping
 public class StatisticUsageApiController implements StatisticUsageApi {
 
     private final StatisticUsageApiDelegate delegate;
 
-    @Autowired
-    public StatisticUsageApiController(StatisticUsageApiDelegate delegate) {
-        this.delegate = delegate;
+    public StatisticUsageApiController(@org.springframework.beans.factory.annotation.Autowired(required = false) StatisticUsageApiDelegate delegate) {
+        this.delegate = Optional.ofNullable(delegate).orElse(new StatisticUsageApiDelegate() {});
     }
 
     @Override
     public StatisticUsageApiDelegate getDelegate() {
         return delegate;
     }
+
 }
--- dist-foreign-ui/src/main/java/com/copyright/rup/dist/foreign/ui/rest/gen/api/StatisticUsageApiDelegate.java
+++ dist-foreign-ui/src/main/java/com/copyright/rup/dist/foreign/ui/rest/gen/api/StatisticUsageApiDelegate.java
@@ -2,38 +2,42 @@ package com.copyright.rup.dist.foreign.ui.rest.gen.api;
 
 import com.copyright.rup.dist.foreign.ui.rest.gen.model.Error;
 import com.copyright.rup.dist.foreign.ui.rest.gen.model.UsageStat;
-    import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.*;
-    import org.slf4j.Logger;
-    import org.slf4j.LoggerFactory;
-    import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-    import org.springframework.validation.annotation.Validated;
+import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.multipart.MultipartFile;
 
-    import javax.validation.Valid;
-    import javax.validation.constraints.*;
-
-    import java.io.IOException;
-
-    import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-    import java.util.Optional;
+import java.util.Map;
+import java.util.Optional;
 
 /**
-* A delegate to be called by the {@link StatisticUsageApiController}}.
-* Implement this interface with a {@link org.springframework.stereotype.Service} annotated class.
-*/
-@Validated
+ * A delegate to be called by the {@link StatisticUsageApiController}}.
+ * Implement this interface with a {@link org.springframework.stereotype.Service} annotated class.
+ */
+
 public interface StatisticUsageApiDelegate {
 
+    default Optional<NativeWebRequest> getRequest() {
+        return Optional.empty();
+    }
+
     /**
-    * @see StatisticUsageApi#getUsageStatistic
-    */
-    ResponseEntity<UsageStat
-> getUsageStatistic(
+     * @see StatisticUsageApi#getUsageStatistic
+     */
+    default ResponseEntity<UsageStat> getUsageStatistic(String usageId) {
+        getRequest().ifPresent(request -> {
+            for (MediaType mediaType: MediaType.parseMediaTypes(request.getHeader("Accept"))) {
+                if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
+                    ApiUtil.setExampleResponse(request, "application/json", "{  \"usageId\" : \"usageId\",  \"matchingMs\" : 0,  \"rightsMs\" : 6,  \"eligibilityMs\" : 1,  \"status\" : \"status\"}");
+                    break;
+                }
+            }
+        });
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
 
-
-String usageId);
+    }
 
 }
