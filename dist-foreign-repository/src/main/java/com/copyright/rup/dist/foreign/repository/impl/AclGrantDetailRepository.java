4da124283920405cef4435e10e2dc36e0f881f95
Aliaksandr_Liakh <Aliaksandr_Liakh1@epam.com>
2022-01-28 04:50:36 -0500

    B-71771 FDA: Grant Set Filter: implement repository

--- dist-foreign-repository/src/main/java/com/copyright/rup/dist/foreign/repository/impl/AclGrantDetailRepository.java
+++ dist-foreign-repository/src/main/java/com/copyright/rup/dist/foreign/repository/impl/AclGrantDetailRepository.java
@@ -1,11 +1,18 @@
 package com.copyright.rup.dist.foreign.repository.impl;
 
 import com.copyright.rup.dist.common.repository.BaseRepository;
+import com.copyright.rup.dist.common.repository.api.Pageable;
+import com.copyright.rup.dist.common.repository.api.Sort;
 import com.copyright.rup.dist.foreign.domain.AclGrantDetail;
+import com.copyright.rup.dist.foreign.domain.AclGrantDetailDto;
+import com.copyright.rup.dist.foreign.domain.filter.AclGrantDetailFilter;
 import com.copyright.rup.dist.foreign.repository.api.IAclGrantDetailRepository;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -28,5 +35,20 @@ public class AclGrantDetailRepository extends BaseRepository implements IAclGran
     @Override
     public List<AclGrantDetail> findByIds(List<String> grantDetailIds) {
         return selectList("IAclGrantDetailMapper.findByIds", Objects.requireNonNull(grantDetailIds));
+    }
+
+    @Override
+    public int findCountByFilter(AclGrantDetailFilter filter) {
+        return selectOne("IAclGrantDetailMapper.findCountByFilter",
+            ImmutableMap.of("filter", Objects.requireNonNull(filter)));
+    }
+
+    @Override
+    public List<AclGrantDetailDto> findDtosByFilter(AclGrantDetailFilter filter, Pageable pageable, Sort sort) {
+        Map<String, Object> parameters = Maps.newHashMapWithExpectedSize(3);
+        parameters.put("filter", Objects.requireNonNull(filter));
+        parameters.put("pageable", pageable);
+        parameters.put("sort", sort);
+        return selectList("IAclGrantDetailMapper.findDtosByFilter", parameters);
     }
 }
