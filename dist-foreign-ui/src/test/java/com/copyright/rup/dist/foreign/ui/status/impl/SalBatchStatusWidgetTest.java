f62c76a9a9f92fbdbac6f3613a74748c8d2a34a9
anton azarenka <anton_azarenka@epam.com>
2022-05-27 14:37:39 +0300

    B-72775 Tech Debt: FDA: improve UI unit tests

--- dist-foreign-ui/src/test/java/com/copyright/rup/dist/foreign/ui/status/impl/SalBatchStatusWidgetTest.java
+++ dist-foreign-ui/src/test/java/com/copyright/rup/dist/foreign/ui/status/impl/SalBatchStatusWidgetTest.java
@@ -9,19 +9,20 @@ import static org.junit.Assert.assertTrue;
 
 import com.copyright.rup.dist.foreign.domain.UsageBatchStatus;
 import com.copyright.rup.dist.foreign.ui.status.api.ICommonBatchStatusController;
+import com.copyright.rup.dist.foreign.ui.usage.UiTestHelper;
+
 import com.vaadin.data.provider.ListDataProvider;
 import com.vaadin.data.provider.Query;
-import com.vaadin.server.Sizeable;
+import com.vaadin.server.Sizeable.Unit;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Grid;
 
+import org.apache.commons.lang3.tuple.Triple;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Verifies {@link SalBatchStatusWidget}.
@@ -51,7 +52,20 @@ public class SalBatchStatusWidgetTest {
         Component component = batchStatusWidget.getComponent(0);
         assertTrue(component instanceof Grid);
         Grid<?> grid = (Grid<?>) component;
-        verifyGrid(grid);
+        UiTestHelper.verifyWindow(grid, null, 100, 100, Unit.PERCENTAGE);
+        assertEquals("batch-status-grid", grid.getId());
+        UiTestHelper.verifyGrid(grid, Arrays.asList(
+            Triple.of("Usage Batch Name", -1.0, -1),
+            Triple.of("Total Count", 130.0, -1),
+            Triple.of("New", 130.0, -1),
+            Triple.of("Work Not Found", 130.0, -1),
+            Triple.of("Work Found", 130.0, -1),
+            Triple.of("Work Not Granted", 130.0, -1),
+            Triple.of("RH Not Found", 130.0, -1),
+            Triple.of("RH Found", 130.0, -1),
+            Triple.of("Eligible", 130.0, -1),
+            Triple.of("Status", -1.0, -1)
+        ));
         assertEquals(0, grid.getDataProvider().size(new Query<>()));
     }
 
@@ -65,21 +79,7 @@ public class SalBatchStatusWidgetTest {
         Component component = batchStatusWidget.getComponent(0);
         assertTrue(component instanceof Grid);
         Grid<?> grid = (Grid<?>) component;
-        verifyGrid(grid);
         assertEquals(1, ((ListDataProvider<?>) grid.getDataProvider()).getItems().size());
         verify(controller);
-    }
-
-    private void verifyGrid(Grid grid) {
-        assertEquals(100, grid.getWidth(), 0);
-        assertEquals(100, grid.getHeight(), 0);
-        assertEquals(Sizeable.Unit.PERCENTAGE, grid.getHeightUnits());
-        assertEquals(Sizeable.Unit.PERCENTAGE, grid.getWidthUnits());
-        assertEquals("batch-status-grid", grid.getId());
-        List<Grid.Column> columns = grid.getColumns();
-        assertEquals(Arrays.asList("Usage Batch Name", "Total Count", "New", "Work Not Found", "Work Found",
-            "Work Not Granted", "RH Not Found", "RH Found", "Eligible", "Status"),
-            columns.stream().map(Grid.Column::getCaption).collect(Collectors.toList()));
-        columns.forEach(column -> assertTrue(column.isSortable()));
     }
 }
