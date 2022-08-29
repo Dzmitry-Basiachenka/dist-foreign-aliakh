4a77a54d102a14ce589de19640af1a158e352fd2
Dzmitry Basiachenka <dzmitry_basiachenka@epam.com>
2022-08-29 17:06:21 +0300

    B-73149 FDA: View Titles by Rightsholder: implement totals row and meta info layout on ACL Scenario Drill Down Titles window

--- dist-foreign-ui/src/main/java/com/copyright/rup/dist/foreign/ui/scenario/impl/acl/calculation/AclScenarioDrillDownTitlesWindow.java
+++ dist-foreign-ui/src/main/java/com/copyright/rup/dist/foreign/ui/scenario/impl/acl/calculation/AclScenarioDrillDownTitlesWindow.java
@@ -5,26 +5,33 @@ import com.copyright.rup.dist.foreign.domain.filter.RightsholderResultsFilter;
 import com.copyright.rup.dist.foreign.ui.main.ForeignUi;
 import com.copyright.rup.dist.foreign.ui.scenario.api.acl.IAclScenarioController;
 import com.copyright.rup.vaadin.ui.Buttons;
+import com.copyright.rup.vaadin.ui.themes.Cornerstone;
 import com.copyright.rup.vaadin.util.CurrencyUtils;
 import com.copyright.rup.vaadin.util.VaadinUtils;
 import com.copyright.rup.vaadin.widget.SearchWidget;
 
 import com.vaadin.data.provider.DataProvider;
 import com.vaadin.data.provider.ListDataProvider;
+import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.Grid.SelectionMode;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
+import com.vaadin.ui.components.grid.FooterCell;
+import com.vaadin.ui.components.grid.FooterRow;
 import com.vaadin.ui.themes.ValoTheme;
 
 import org.apache.commons.lang3.StringUtils;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 /**
  * Modal window that provides functionality for viewing wr wrk insts and titles by rightsholder.
@@ -49,10 +56,11 @@ public class AclScenarioDrillDownTitlesWindow extends Window implements SearchWi
 
     private final IAclScenarioController controller;
     private final RightsholderResultsFilter filter;
+    private final SearchWidget searchWidget;
 
-    private SearchWidget searchWidget;
     private ListDataProvider<AclRightsholderTotalsHolderDto> dataProvider;
     private Grid<AclRightsholderTotalsHolderDto> grid;
+    private List<AclRightsholderTotalsHolderDto> aclRightsholderTotalsHolderDtos;
 
     /**
      * Constructor.
@@ -65,16 +73,49 @@ public class AclScenarioDrillDownTitlesWindow extends Window implements SearchWi
         this.filter = filter;
         setWidth(1280, Unit.PIXELS);
         setHeight(600, Unit.PIXELS);
+        searchWidget = new SearchWidget(this);
+        searchWidget.setPrompt(ForeignUi.getMessage("field.prompt.view_titles_by_rightsholder.search.acl"));
         initGrid();
-        // TODO {dbasiachenka} implement label section
         Button closeButton = Buttons.createCloseButton(this);
-        VerticalLayout layout = new VerticalLayout(initSearchWidget(), grid, closeButton);
+        VerticalLayout layout = new VerticalLayout(initMetaInfoLayout(), searchWidget, grid, closeButton);
         layout.setSizeFull();
         layout.setExpandRatio(grid, 1);
         layout.setComponentAlignment(closeButton, Alignment.BOTTOM_RIGHT);
         setContent(layout);
         setCaption(ForeignUi.getMessage("window.acl_scenario_drill_down_titles"));
         VaadinUtils.addComponentStyle(this, "acl-scenario-drill-down-titles-window");
+    }
+
+    private VerticalLayout initMetaInfoLayout() {
+        VerticalLayout verticalLayout = new VerticalLayout();
+        HorizontalLayout rhAccountLayout = initLabelsHorizontalLayout("RH Account #", filter.getRhAccountNumber());
+        HorizontalLayout rhNameLayout = initLabelsHorizontalLayout("RH Name", filter.getRhName());
+        HorizontalLayout aggLcClassIdLayout = initLabelsHorizontalLayout("Agg Lic Class ID",
+            filter.getAggregateLicenseeClassId());
+        HorizontalLayout aggLcClassNameLayout = initLabelsHorizontalLayout("Agg Lic Class Name",
+            filter.getAggregateLicenseeClassName());
+        HorizontalLayout[] components =
+            Stream.of(rhAccountLayout, rhNameLayout, aggLcClassIdLayout, aggLcClassNameLayout)
+                .filter(Objects::nonNull)
+                .toArray(HorizontalLayout[]::new);
+        verticalLayout.setMargin(new MarginInfo(true, false, true, false));
+        verticalLayout.setSpacing(false);
+        verticalLayout.addComponents(components);
+        return verticalLayout;
+    }
+
+    private HorizontalLayout initLabelsHorizontalLayout(String title, Object value) {
+        HorizontalLayout horizontalLayout = null;
+        if (Objects.nonNull(value)) {
+            horizontalLayout = new HorizontalLayout();
+            Label titleLabel = new Label(ForeignUi.getMessage("label.title", title));
+            titleLabel.setWidth(125, Unit.PIXELS);
+            titleLabel.addStyleName(Cornerstone.LABEL_BOLD);
+            Label valueLabel = new Label(String.valueOf(value));
+            valueLabel.setWidth(1125, Unit.PIXELS);
+            horizontalLayout.addComponents(titleLabel, valueLabel);
+        }
+        return horizontalLayout;
     }
 
     @Override
@@ -91,7 +132,8 @@ public class AclScenarioDrillDownTitlesWindow extends Window implements SearchWi
     }
 
     private void initGrid() {
-        dataProvider = DataProvider.ofCollection(controller.getRightsholderTitleResults(filter));
+        aclRightsholderTotalsHolderDtos = controller.getRightsholderTitleResults(filter);
+        dataProvider = DataProvider.ofCollection(aclRightsholderTotalsHolderDtos);
         grid = new Grid<>(dataProvider);
         addGridColumns();
         addFooter();
@@ -143,16 +185,27 @@ public class AclScenarioDrillDownTitlesWindow extends Window implements SearchWi
     }
 
     private void addFooter() {
-        // TODO {dbasiachenka} implement
+        grid.appendFooterRow();
+        grid.setFooterVisible(true);
+        FooterRow row = grid.getFooterRow(0);
+        row.setStyleName("table-ext-footer");
+        row.join(WR_WRK_INST_PROPERTY, SYSTEM_TITLE_PROPERTY).setText("Totals");
+        initFooterAmountCell(row, PRINT_GROSS_TOTAL_PROPERTY, AclRightsholderTotalsHolderDto::getGrossTotalPrint);
+        initFooterAmountCell(row, PRINT_NET_TOTAL_PROPERTY, AclRightsholderTotalsHolderDto::getNetTotalPrint);
+        initFooterAmountCell(row, DIGITAL_GROSS_TOTAL_PROPERTY, AclRightsholderTotalsHolderDto::getGrossTotalDigital);
+        initFooterAmountCell(row, DIGITAL_NET_TOTAL_PROPERTY, AclRightsholderTotalsHolderDto::getNetTotalDigital);
+        initFooterAmountCell(row, GROSS_TOTAL_PROPERTY, AclRightsholderTotalsHolderDto::getGrossTotal);
+        initFooterAmountCell(row, NET_TOTAL_PROPERTY, AclRightsholderTotalsHolderDto::getNetTotal);
     }
 
-    private HorizontalLayout initSearchWidget() {
-        searchWidget = new SearchWidget(this);
-        searchWidget.setPrompt(ForeignUi.getMessage("field.prompt.view_titles_by_rightsholder.search.acl"));
-        searchWidget.setWidth(60, Unit.PERCENTAGE);
-        HorizontalLayout layout = new HorizontalLayout(searchWidget);
-        layout.setWidth(100, Unit.PERCENTAGE);
-        layout.setComponentAlignment(searchWidget, Alignment.MIDDLE_CENTER);
-        return layout;
+    private void initFooterAmountCell(FooterRow row, String cellProperty,
+                                      Function<AclRightsholderTotalsHolderDto, BigDecimal> function) {
+        FooterCell cell = row.getCell(cellProperty);
+        cell.setText(CurrencyUtils.format(
+            aclRightsholderTotalsHolderDtos.stream()
+                .map(function)
+                .reduce(BigDecimal.ZERO, BigDecimal::add),
+            null));
+        cell.setStyleName(STYLE_ALIGN_RIGHT);
     }
 }
--- dist-foreign-ui/src/test/java/com/copyright/rup/dist/foreign/ui/scenario/impl/acl/calculation/AclScenarioDrillDownTitlesWindowTest.java
+++ dist-foreign-ui/src/test/java/com/copyright/rup/dist/foreign/ui/scenario/impl/acl/calculation/AclScenarioDrillDownTitlesWindowTest.java
@@ -1,8 +1,10 @@
 package com.copyright.rup.dist.foreign.ui.scenario.impl.acl.calculation;
 
 import static com.copyright.rup.dist.foreign.ui.usage.UiTestHelper.verifyButton;
+import static com.copyright.rup.dist.foreign.ui.usage.UiTestHelper.verifyFooterItems;
 import static com.copyright.rup.dist.foreign.ui.usage.UiTestHelper.verifyGrid;
 import static com.copyright.rup.dist.foreign.ui.usage.UiTestHelper.verifyGridItems;
+import static com.copyright.rup.dist.foreign.ui.usage.UiTestHelper.verifyLabel;
 import static com.copyright.rup.dist.foreign.ui.usage.UiTestHelper.verifyWindow;
 
 import static org.easymock.EasyMock.expect;
@@ -20,8 +22,7 @@ import com.copyright.rup.dist.foreign.ui.scenario.api.acl.IAclScenarioController
 import com.copyright.rup.vaadin.widget.SearchWidget;
 
 import com.vaadin.server.Sizeable;
-import com.vaadin.server.Sizeable.Unit;
-import com.vaadin.ui.Alignment;
+import com.vaadin.shared.ui.ContentMode;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.HorizontalLayout;
@@ -36,6 +37,7 @@ import org.powermock.reflect.Whitebox;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.stream.IntStream;
 
 /**
  * Verifies {@link AclScenarioDrillDownTitlesWindow}.
@@ -51,20 +53,19 @@ public class AclScenarioDrillDownTitlesWindowTest {
     private static final String SCENARIO_UID = "1236249b-0063-4f7e-888a-3b1051d7a898";
     private static final Long RH_ACCOUNT_NUMBER = 1000010022L;
     private static final String RH_NAME = "Yale University Press";
+    private static final String AGG_LIC_CLASS_NAME = "Food and Tobacco";
+    private static final String STYLE_ALIGN_RIGHT = "v-align-right";
 
     private AclScenarioDrillDownTitlesWindow window;
-    private Grid<AclRightsholderTotalsHolderDto> grid;
 
     @Before
     public void setUp() {
         IAclScenarioController controller = createMock(IAclScenarioController.class);
-        grid = createMock(Grid.class);
         RightsholderResultsFilter rightsholderResultsFilter = buildRightsholderResultsFilter();
         expect(controller.getRightsholderTitleResults(rightsholderResultsFilter))
             .andReturn(Collections.singletonList(buildAclRightsholderTotalsHolderDto()));
         replay(controller);
         window = new AclScenarioDrillDownTitlesWindow(controller, rightsholderResultsFilter);
-        Whitebox.setInternalState(window, grid);
         verify(controller);
         reset(controller);
     }
@@ -73,9 +74,10 @@ public class AclScenarioDrillDownTitlesWindowTest {
     public void testStructure() {
         verifyWindow(window, "Results by Rightsholder: Title", 1280, 600, Sizeable.Unit.PIXELS);
         VerticalLayout content = (VerticalLayout) window.getContent();
-        assertEquals(3, content.getComponentCount());
-        verifySearchWidget(content.getComponent(0));
-        Component component = content.getComponent(1);
+        assertEquals(4, content.getComponentCount());
+        verifyMetaInfoLayout(content.getComponent(0));
+        verifySearchWidget(content.getComponent(1));
+        Component component = content.getComponent(2);
         assertTrue(component instanceof Grid);
         verifyGrid((Grid) component, Arrays.asList(
             Triple.of("Wr Wrk Inst", 110.0, -1),
@@ -87,15 +89,17 @@ public class AclScenarioDrillDownTitlesWindowTest {
             Triple.of("Total Gross Amt", 150.0, -1),
             Triple.of("Total Net Amt", 140.0, -1)
         ));
-        verifyButton(content.getComponent(2), "Close", true);
+        verifyButton(content.getComponent(3), "Close", true);
         assertEquals("acl-scenario-drill-down-titles-window", window.getStyleName());
         assertEquals("acl-scenario-drill-down-titles-window", window.getId());
     }
 
     @Test
     public void testPerformSearch() {
+        Grid grid = createMock(Grid.class);
         SearchWidget searchWidget = createMock(SearchWidget.class);
         Whitebox.setInternalState(window, searchWidget);
+        Whitebox.setInternalState(window, grid);
         expect(searchWidget.getSearchValue()).andReturn("Search").once();
         grid.recalculateColumnWidths();
         expectLastCall().once();
@@ -108,25 +112,20 @@ public class AclScenarioDrillDownTitlesWindowTest {
     public void testGridValues() {
         VerticalLayout content = (VerticalLayout) window.getContent();
         Object[][] expectedCells = {
-            {127778306L, "Adaptations", "2,000.00", "8,300.00", "1,000.00", "2,000.00", "3,000.00", "10,300.00"}
+            {127778306L, "Adaptations", "1,500.00", "8,300.00", "1,000.00", "2,000.00", "2,500.00", "10,300.00"}
         };
-        verifyGridItems((Grid) content.getComponent(1),
-            Collections.singletonList(buildAclRightsholderTotalsHolderDto()), expectedCells);
-    }
+        Grid grid = (Grid) content.getComponent(2);
+        verifyGridItems(grid, Collections.singletonList(buildAclRightsholderTotalsHolderDto()), expectedCells);
+        Object[][] expectedFooterColumns = {
+            {"grossTotalPrint", "1,500.00", STYLE_ALIGN_RIGHT},
+            {"netTotalPrint", "8,300.00", STYLE_ALIGN_RIGHT},
+            {"grossTotalDigital", "1,000.00", STYLE_ALIGN_RIGHT},
+            {"netTotalDigital", "2,000.00", STYLE_ALIGN_RIGHT},
+            {"grossTotal", "2,500.00", STYLE_ALIGN_RIGHT},
+            {"netTotal", "10,300.00", STYLE_ALIGN_RIGHT},
 
-    private void verifySearchWidget(Component component) {
-        assertTrue(component instanceof HorizontalLayout);
-        HorizontalLayout horizontalLayout = (HorizontalLayout) component;
-        assertEquals(1, horizontalLayout.getComponentCount());
-        SearchWidget searchWidget = (SearchWidget) horizontalLayout.getComponent(0);
-        assertEquals(60, searchWidget.getWidth(), 0);
-        assertEquals(Unit.PERCENTAGE, searchWidget.getWidthUnits());
-        assertEquals(Alignment.MIDDLE_CENTER, horizontalLayout.getComponentAlignment(searchWidget));
-        assertTrue(horizontalLayout.isSpacing());
-        assertEquals(100, horizontalLayout.getWidth(), 0);
-        assertEquals(Unit.PERCENTAGE, horizontalLayout.getWidthUnits());
-        assertEquals("Enter Wr Wrk Inst or System Title",
-            ((TextField) searchWidget.getComponent(0)).getPlaceholder());
+        };
+        verifyFooterItems(grid, expectedFooterColumns);
     }
 
     private RightsholderResultsFilter buildRightsholderResultsFilter() {
@@ -134,6 +133,8 @@ public class AclScenarioDrillDownTitlesWindowTest {
         filter.setScenarioId(SCENARIO_UID);
         filter.setRhAccountNumber(RH_ACCOUNT_NUMBER);
         filter.setRhName(RH_NAME);
+        filter.setAggregateLicenseeClassId(1);
+        filter.setAggregateLicenseeClassName(AGG_LIC_CLASS_NAME);
         return filter;
     }
 
@@ -141,14 +142,45 @@ public class AclScenarioDrillDownTitlesWindowTest {
         AclRightsholderTotalsHolderDto holder = new AclRightsholderTotalsHolderDto();
         holder.getRightsholder().setAccountNumber(RH_ACCOUNT_NUMBER);
         holder.getRightsholder().setName(RH_NAME);
-        holder.setGrossTotalPrint(new BigDecimal("2000.0000000001"));
+        holder.setGrossTotalPrint(new BigDecimal("1500.0000000001"));
         holder.setNetTotalPrint(new BigDecimal("8300.0000000001"));
         holder.setGrossTotalDigital(new BigDecimal("1000.0000000001"));
         holder.setNetTotalDigital(new BigDecimal("2000.0000000001"));
         holder.setWrWrkInst(127778306L);
         holder.setSystemTitle("Adaptations");
-        holder.setGrossTotal(new BigDecimal("3000.0000000002"));
+        holder.setGrossTotal(new BigDecimal("2500.0000000002"));
         holder.setNetTotal(new BigDecimal("10300.0000000002"));
         return holder;
+    }
+
+    private void verifyMetaInfoLayout(Component component) {
+        assertTrue(component instanceof VerticalLayout);
+        VerticalLayout verticalLayout = (VerticalLayout) component;
+        assertEquals(4, verticalLayout.getComponentCount());
+        String[][] expectedMetaInfoCaptions = {
+            {"RH Account #:", "1000010022"},
+            {"RH Name:", RH_NAME},
+            {"Agg Lic Class ID:", "1"},
+            {"Agg Lic Class Name:", AGG_LIC_CLASS_NAME}
+        };
+        verifyLabelLayout(verticalLayout, expectedMetaInfoCaptions, verticalLayout.getComponentCount());
+    }
+
+    private void verifyLabelLayout(VerticalLayout verticalLayout, String[][] expectedCaptions, int componentCount) {
+        IntStream.range(0, componentCount).forEach(i -> {
+            Component horizontalComponent = verticalLayout.getComponent(i);
+            assertTrue(horizontalComponent instanceof HorizontalLayout);
+            HorizontalLayout horizontalLayout = (HorizontalLayout) verticalLayout.getComponent(i);
+            assertEquals(2, horizontalLayout.getComponentCount());
+            verifyLabel(horizontalLayout.getComponent(0), expectedCaptions[i][0], ContentMode.TEXT, 125);
+            verifyLabel(horizontalLayout.getComponent(1), expectedCaptions[i][1], ContentMode.TEXT, 1125);
+        });
+    }
+
+    private void verifySearchWidget(Component component) {
+        assertTrue(component instanceof SearchWidget);
+        SearchWidget searchWidget = (SearchWidget) component;
+        assertEquals("Enter Wr Wrk Inst or System Title",
+            Whitebox.getInternalState(searchWidget, TextField.class).getPlaceholder());
     }
 }
