de4ccb8d381fccf53ff2684247b21c5283133a23
Mikita Maistrenka <mikita_maistrenka@epam.com>
2022-12-28 09:22:32 +0300

    B-68612 FDA: Load ACLCI fund pool: implement Liquibase script to save ACLCI fund pool fields

--- dist-foreign-db/src/main/liquibase/com/copyright/rup/dist/foreign/db/changelog-16.groovy
+++ dist-foreign-db/src/main/liquibase/com/copyright/rup/dist/foreign/db/changelog-16.groovy
@@ -153,4 +153,16 @@ databaseChangeLog {
             modifyDataType(schemaName: dbAppsSchema, tableName: 'df_usage_aclci', columnName: 'coverage_period', newDataType: 'VARCHAR(9)')
         }
     }
+
+    changeSet(id: '2020-12-28-00', author: 'Mikita Maistrenka <mmaistrenka@copyright.com>') {
+        comment("B-68577 FDA: Load ACLCI fund pool: add aclci_fields column to df_fund_pool")
+
+        addColumn(schemaName: dbAppsSchema, tableName: 'df_fund_pool') {
+            column(name: 'aclci_fields', type: 'JSONB', remarks: 'The fields of ACLCI fund pool')
+        }
+
+        rollback {
+            dropColumn(schemaName: dbAppsSchema, tableName: 'df_fund_pool', columnName: 'aclci_fields')
+        }
+    }
 }
