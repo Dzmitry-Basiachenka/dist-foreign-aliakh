edd66afa4d1bc756d2a18e5ad48f1e260d60e175
Aliaksandr_Liakh <Aliaksandr_Liakh1@epam.com>
2023-04-17 04:20:54 -0400

    B-78666 FDA: Add columns to NTS WD Batch Summary Report: add number and gross amount columns for details with status TO_BE_DISTRIBUTED

--- dist-foreign-db/src/main/liquibase/com/copyright/rup/dist/foreign/db/changelog-17.groovy
+++ dist-foreign-db/src/main/liquibase/com/copyright/rup/dist/foreign/db/changelog-17.groovy
@@ -38,4 +38,71 @@ databaseChangeLog {
             dropColumn(schemaName: dbAppsSchema, tableName: 'df_usage_fas', columnName: 'reported_standard_number')
         }
     }
+
+    changeSet(id: '2023-04-19-00', author: 'Aliaksandr Liakh <aliakh@copyright.com>') {
+        comment("B-78666 FDA: Add columns to NTS WD Batch Summary Report: add columns to view v_nts_withdrawn_batch_summary_report")
+
+        dropView(viewName: 'v_nts_withdrawn_batch_summary_report', schemaName: dbAppsSchema)
+
+        createView(viewName: 'v_nts_withdrawn_batch_summary_report', schemaName: dbAppsSchema) {
+            """select
+                rro.name rro_name,
+                b.rro_account_number,
+                b.name as batch_name,
+                b.payment_date,
+                b.gross_amount,
+                sum(case when u.status_ind = 'NTS_WITHDRAWN' then 1 else 0 end) as nts_details_count,
+                sum(case when u.status_ind = 'NTS_WITHDRAWN' then u.gross_amount else 0 end) as nts_details_gross_amount,
+                sum(case when u.status_ind = 'TO_BE_DISTRIBUTED' then 1 else 0 end) as to_be_distributed_details_count,
+                sum(case when u.status_ind = 'TO_BE_DISTRIBUTED' then u.gross_amount else 0 end) as to_be_distributed_details_gross_amount,
+                b.created_datetime
+            from ${dbAppsSchema}.df_usage u
+            join ${dbAppsSchema}.df_usage_batch b on b.df_usage_batch_uid = u.df_usage_batch_uid
+            left join ${dbAppsSchema}.df_rightsholder rro on rro.rh_account_number = b.rro_account_number
+            where u.status_ind in ('NTS_WITHDRAWN', 'TO_BE_DISTRIBUTED')
+            group by rro.name, b.rro_account_number, b.name, b.payment_date, b.gross_amount, b.created_datetime"""
+        }
+
+        rollback {
+            dropView(viewName: 'v_nts_withdrawn_batch_summary_report', schemaName: dbAppsSchema)
+
+            createView(viewName: 'v_nts_withdrawn_batch_summary_report', schemaName: dbAppsSchema) {
+                """select
+                    rro.name rro_name,
+                    rro.rh_account_number rro_account_number,
+                    report.batch_name,
+                    report.payment_date,
+                    report.gross_amount,
+                    report.nts_details_count,
+                    report.nts_details_gross_amount,
+                    report.created_datetime
+                from (
+                    select
+                        nested_report.rro_account_number,
+                        nested_report.batch_name as batch_name,
+                        nested_report.payment_date,
+                        nested_report.gross_amount,
+                        sum(nested_report.nts_details_count) nts_details_count,
+                        sum(nested_report.nts_details_gross_amount) nts_details_gross_amount,
+                        nested_report.created_datetime
+                    from (
+                        select
+                            b.rro_account_number,
+                            b.name as batch_name,
+                            b.payment_date,
+                            b.gross_amount,
+                            count(1) as nts_details_count,
+                            sum(u.gross_amount) as nts_details_gross_amount,
+                            b.created_datetime
+                        from ${dbAppsSchema}.df_usage u
+                        join ${dbAppsSchema}.df_usage_batch b on b.df_usage_batch_uid = u.df_usage_batch_uid
+                        where u.status_ind = 'NTS_WITHDRAWN'
+                        group by b.rro_account_number, b.name, b.payment_date, b.gross_amount, u.product_family, b.created_datetime
+                    ) as nested_report
+                    group by nested_report.rro_account_number, nested_report.batch_name, nested_report.payment_date, nested_report.gross_amount, nested_report.created_datetime
+                ) as report
+                left join ${dbAppsSchema}.df_rightsholder rro on rro.rh_account_number = report.rro_account_number"""
+            }
+        }
+    }
 }
