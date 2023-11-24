33d3ac9bed52e803c77fa0fb79c27d5da19a0a29
Dzmitry Basiachenka <dzmitry_basiachenka@epam.com>
2023-11-24 15:30:00 +0300

    CDP-1630 FDA: Implement improvements from SAR Review: fix issue 'Class has a circular dependency with other classes' for ConfirmActionDialogWindow

---
+++ dist-foreign-vui/src/main/java/com/copyright/rup/dist/foreign/vui/vaadin/common/ui/component/window/ConfirmWindows.java
@@ -0,0 +1,60 @@
+package com.copyright.rup.dist.foreign.vui.vaadin.common.ui.component.window;
+
+import com.vaadin.flow.data.binder.Validator;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+/**
+ * Utility class to work with confirm modal windows.
+ * <p/>
+ * Copyright (C) 2023 copyright.com
+ * <p/>
+ * Date: 11/22/2023
+ *
+ * @author Aliaksandr Liakh
+ */
+public final class ConfirmWindows {
+
+    private ConfirmWindows() {
+        throw new AssertionError("Constructor shouldn't be called directly");
+    }
+
+    /**
+     * Shows confirm action dialog window.
+     *
+     * @param caption        caption of dialog
+     * @param message        message of dialog
+     * @param confirmCaption caption for confirm button
+     * @param declineCaption caption for decline button
+     * @param action         action on confirmation button click
+     */
+    public static void showConfirmDialogWithReason(String caption, String message, String confirmCaption,
+                                                   String declineCaption,
+                                                   Consumer<String> action) {
+        showConfirmDialogWithReason(caption, message, confirmCaption, declineCaption, action,
+            Collections.emptyList());
+    }
+
+    /**
+     * Shows confirm action dialog window.
+     *
+     * @param caption        caption of dialog
+     * @param message        message of dialog
+     * @param confirmCaption caption for confirm button
+     * @param declineCaption caption for decline button
+     * @param action         action on confirmation button click
+     * @param validators     list of validators for reason text field
+     */
+    public static void showConfirmDialogWithReason(String caption, String message, String confirmCaption,
+                                                   String declineCaption,
+                                                   Consumer<String> action,
+                                                   List<Validator<String>> validators) {
+        Objects.requireNonNull(validators);
+        ConfirmActionDialogWindow window =
+            new ConfirmActionDialogWindow(action, caption, message, confirmCaption, declineCaption, validators);
+        Windows.showModalWindow(window);
+    }
+}
--- dist-foreign-vui/src/test/java/com/copyright/rup/dist/foreign/vui/vaadin/common/ui/component/window/WindowsTest.java
+++ dist-foreign-vui/src/test/java/com/copyright/rup/dist/foreign/vui/vaadin/common/ui/component/window/WindowsTest.java
@@ -7,7 +7,6 @@ import static org.easymock.EasyMock.newCapture;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
 import static org.powermock.api.easymock.PowerMock.createMock;
 import static org.powermock.api.easymock.PowerMock.createNicePartialMock;
 import static org.powermock.api.easymock.PowerMock.createPartialMock;
@@ -23,18 +22,14 @@ import com.copyright.rup.dist.foreign.vui.vaadin.common.ui.component.filter.IFil
 import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.data.binder.Validator;
 
 import org.easymock.Capture;
-import org.easymock.EasyMock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.Collections;
-import java.util.List;
-import java.util.function.Consumer;
 
 /**
  * Verifies {@link Windows} utility class.
@@ -52,9 +47,6 @@ public class WindowsTest {
     private static final String WINDOW_CAPTION = "caption";
     private static final String WINDOW_MESSAGE = "message";
     private static final String SHOW_MODAL_WINDOW_METHOD_NAME = "showModalWindow";
-    private static final String SHOW_CONFIRM_DIALOG_WITH_REASON_METHOD_NAME = "showConfirmDialogWithReason";
-    private static final String WINDOW_CONFIRM_CAPTION = "confirmCaption";
-    private static final String WINDOW_DECLINE_CAPTION = "declineCaption";
 
     @Test(expected = NullPointerException.class)
     public void testShowNullModalWindow() {
@@ -119,61 +111,5 @@ public class WindowsTest {
         assertNotNull(filterWindow);
         assertEquals(caption, filterWindow.getHeaderTitle());
         verify(controller, Windows.class);
-    }
-
-    @Test
-    public void testShowConfirmDialogWithReason() {
-        Consumer<String> reasonConsumer = EasyMock.createMock(Consumer.class);
-        Capture<Dialog> dialogCapture = newCapture();
-        createPartialMock(Windows.class, SHOW_MODAL_WINDOW_METHOD_NAME, Dialog.class);
-        Windows.showModalWindow(capture(dialogCapture));
-        expectLastCall().once();
-        replay(Windows.class);
-        Windows.showConfirmDialogWithReason(null, WINDOW_MESSAGE, null, null,  reasonConsumer);
-        verify(Windows.class);
-        assertEquals(ConfirmActionDialogWindow.class, dialogCapture.getValue().getClass());
-    }
-
-    @Test
-    public void testShowConfirmDialogWithoutValidator() {
-        Consumer<String> reasonConsumer = EasyMock.createMock(Consumer.class);
-        Capture<String> messageCapture = newCapture();
-        Capture<Consumer<String>> reasonConsumerCapture = newCapture();
-        Capture<List<Validator<String>>> validatorsCapture = newCapture();
-        createPartialMock(Windows.class, SHOW_CONFIRM_DIALOG_WITH_REASON_METHOD_NAME, String.class, String.class,
-            String.class, String.class, Consumer.class, List.class);
-        Windows.showConfirmDialogWithReason(anyObject(String.class), capture(messageCapture),
-            anyObject(String.class), anyObject(String.class), capture(reasonConsumerCapture),
-            capture(validatorsCapture));
-        expectLastCall().once();
-        replay(Windows.class);
-        Windows.showConfirmDialogWithReason(WINDOW_CAPTION, WINDOW_MESSAGE, WINDOW_CONFIRM_CAPTION,
-            WINDOW_DECLINE_CAPTION, reasonConsumer);
-        verify(Windows.class);
-        assertSame(WINDOW_MESSAGE, messageCapture.getValue());
-        assertSame(reasonConsumer, reasonConsumerCapture.getValue());
-        assertTrue(validatorsCapture.getValue().isEmpty());
-    }
-
-    @Test
-    public void testShowConfirmDialogWithValidators() {
-        Consumer<String> reasonConsumer = EasyMock.createMock(Consumer.class);
-        List<Validator<String>> validators = Collections.singletonList(createMock(Validator.class));
-        Capture<String> messageCapture = newCapture();
-        Capture<Consumer<String>> reasonConsumerCapture = newCapture();
-        Capture<List<Validator<String>>> validatorsCapture = newCapture();
-        createPartialMock(Windows.class, SHOW_CONFIRM_DIALOG_WITH_REASON_METHOD_NAME, String.class, String.class,
-            String.class, String.class, Consumer.class, List.class);
-        Windows.showConfirmDialogWithReason(anyObject(String.class), capture(messageCapture),
-            anyObject(String.class), anyObject(String.class), capture(reasonConsumerCapture),
-            capture(validatorsCapture));
-        expectLastCall().once();
-        replay(Windows.class);
-        Windows.showConfirmDialogWithReason(WINDOW_CAPTION, WINDOW_MESSAGE, WINDOW_CONFIRM_CAPTION,
-            WINDOW_DECLINE_CAPTION, reasonConsumer, validators);
-        verify(Windows.class);
-        assertSame(WINDOW_MESSAGE, messageCapture.getValue());
-        assertSame(reasonConsumer, reasonConsumerCapture.getValue());
-        assertSame(validators, validatorsCapture.getValue());
     }
 }
