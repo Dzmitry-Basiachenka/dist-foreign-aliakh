5cd8309626ad29c9606114c93d27add52b0fdc27
Aliaksandr_Liakh <Aliaksandr_Liakh1@epam.com>
2024-01-09 04:03:03 -0500

    CDP-1672 FDA: Vaadin Migration: FAS/FAS2 - Usages Tab: migrate validators and formatters

---
+++ dist-foreign-vui/src/main/java/com/copyright/rup/dist/foreign/vui/common/utils/BigDecimalUtils.java
@@ -0,0 +1,50 @@
+package com.copyright.rup.dist.foreign.vui.common.utils;
+
+import com.copyright.rup.dist.foreign.vui.vaadin.common.util.CurrencyUtils;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+/**
+ * Class for conversions between {@link BigDecimal} and {@link String}.
+ * <p>
+ * Copyright (C) 2021 copyright.com
+ * <p>
+ * Date: 12/09/2021
+ *
+ * @author Aliaksandr Liakh
+ */
+public final class BigDecimalUtils {
+
+    private static final DecimalFormat CURRENCY_FORMATTER_FOR_GRID = new DecimalFormat("#,##0.00########",
+        CurrencyUtils.getParameterizedDecimalFormatSymbols());
+
+    private static final DecimalFormat CURRENCY_FORMATTER_FOR_DIALOG = new DecimalFormat("0.00########",
+        CurrencyUtils.getParameterizedDecimalFormatSymbols());
+
+    private BigDecimalUtils() {
+        throw new AssertionError("Constructor shouldn't be called directly");
+    }
+
+    /**
+     * Formats decimal amount without trailing zeros after the second digit after the decimal point
+     * for grid (with grouping separators).
+     *
+     * @param value instance of {@link BigDecimal}
+     * @return formatted string or empty string in case if amount is null
+     */
+    public static String formatCurrencyForGrid(BigDecimal value) {
+        return CurrencyUtils.format(value, CURRENCY_FORMATTER_FOR_GRID);
+    }
+
+    /**
+     * Formats decimal amount without trailing zeros after the second digit after the decimal point
+     * for dialog (without grouping separators).
+     *
+     * @param value instance of {@link BigDecimal}
+     * @return formatted string or empty string in case if amount is null
+     */
+    public static String formatCurrencyForDialog(BigDecimal value) {
+        return CurrencyUtils.format(value, CURRENCY_FORMATTER_FOR_DIALOG);
+    }
+}
---
+++ dist-foreign-vui/src/main/java/com/copyright/rup/dist/foreign/vui/common/utils/IDateFormatter.java
@@ -0,0 +1,84 @@
+package com.copyright.rup.dist.foreign.vui.common.utils;
+
+import com.copyright.rup.common.date.RupDateUtils;
+import com.copyright.rup.dist.common.util.CommonDateUtils;
+
+import com.vaadin.flow.function.ValueProvider;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.FastDateFormat;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.function.Function;
+
+/**
+ * Class for conversions between dates and {@link String}.
+ * <p>
+ * Copyright (C) 2021 copyright.com
+ * <p>
+ * Date: 10/18/2021
+ *
+ * @author Aliaksandr Liakh
+ */
+public interface IDateFormatter {
+
+    /**
+     * Formats instance of {@link LocalDate} to {@link String} by pattern "MM/dd/yyyy".
+     *
+     * @param date instance of {@link LocalDate}
+     * @return formatted date string if date is not {@code null}, otherwise empty string
+     */
+    default String toShortFormat(LocalDate date) {
+        return CommonDateUtils.format(date, RupDateUtils.US_DATE_FORMAT_PATTERN_SHORT);
+    }
+
+    /**
+     * Gets function that formats instance of {@link LocalDate} to {@link String} by pattern "MM/dd/yyyy".
+     *
+     * @param getter bean getter
+     * @return function that formats date to string if date is not {@code null}, otherwise to empty string
+     * @param <T> bean type
+     */
+    default <T> ValueProvider<T, String> toShortFormat(Function<T, LocalDate> getter) {
+        return bean -> toShortFormat(getter.apply(bean));
+    }
+
+    /**
+     * Formats instance of {@link Date} to {@link String} by pattern "MM/dd/yyyy".
+     *
+     * @param date instance of {@link Date}
+     * @return formatted date string if date is not {@code null}, otherwise empty string
+     */
+    default String toShortFormat(Date date) {
+        return Objects.nonNull(date)
+            ? FastDateFormat.getInstance(RupDateUtils.US_DATE_FORMAT_PATTERN_SHORT).format(date)
+            : StringUtils.EMPTY;
+    }
+
+    /**
+     * Gets function that formats instance of {@link Date} to {@link String} by pattern "MM/dd/yyyy".
+     *
+     * @param getter bean getter
+     * @return function that formats date to string if date is not {@code null}, otherwise to empty string
+     * @param <T> bean type
+     */
+    default <T> ValueProvider<T, String> toShortFormat(com.google.common.base.Function<T, Date> getter) {
+        return bean -> toShortFormat(getter.apply(bean));
+    }
+
+    /**
+     * Formats instance of {@link Date} to {@link String} by pattern "MM/dd/yyyy h:mm a".
+     *
+     * @param date instance of {@link Date}
+     * @return formatted date string if date is not {@code null}, otherwise empty string
+     */
+    default String toLongFormat(Date date) {
+        return Objects.nonNull(date)
+            ? new SimpleDateFormat(RupDateUtils.US_DATETIME_FORMAT_PATTERN_LONG, Locale.getDefault()).format(date)
+            : StringUtils.EMPTY;
+    }
+}
