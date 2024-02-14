3bccfd7d1e1ddf3e6a1e72f8f8f96559149a8f76
Ihar Suvorau <ihar_suvorau@epam.com>
2024-02-14 12:52:12 +0400

    CDP-2232 Tech Debt: FDA: remove unused import

--- dist-foreign-vui/src/test/java/com/copyright/rup/dist/foreign/vui/usage/impl/nts/NtsUsageWidgetTest.java
+++ dist-foreign-vui/src/test/java/com/copyright/rup/dist/foreign/vui/usage/impl/nts/NtsUsageWidgetTest.java
@@ -39,9 +39,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
 import com.vaadin.flow.component.ClickEvent;
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.ComponentUtil;
 import com.vaadin.flow.component.UI;
